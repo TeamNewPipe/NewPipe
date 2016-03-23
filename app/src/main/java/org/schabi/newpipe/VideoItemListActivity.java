4f8e4ca0ad577b0435cef00c77ed841e19e7f2fc
Hayden <haydenlueck@gmail.com>
2016-03-22 19:53:55 -0600

    Got rid of useless Toast from testing

--- app/src/main/java/org/schabi/newpipe/VideoItemListActivity.java
+++ app/src/main/java/org/schabi/newpipe/VideoItemListActivity.java
@@ -14,7 +14,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
