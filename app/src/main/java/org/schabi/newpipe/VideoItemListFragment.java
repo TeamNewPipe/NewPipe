ddc3b47dfa0e720382933dd1664d2c8790e9d588
David <david.satiricon@gmail.com>
2016-06-07 11:43:13 -0300

    Fix for #308 part 2

--- app/src/main/java/org/schabi/newpipe/VideoItemListFragment.java
+++ app/src/main/java/org/schabi/newpipe/VideoItemListFragment.java
@@ -289,7 +289,7 @@ public class VideoItemListFragment extends ListFragment {
                                  int visibleItemCount, int totalItemCount) {
                 if (mode != PRESENT_VIDEOS_MODE
                         && list.getChildAt(0) != null
-                        && list.getLastVisiblePosition() == list.getAdapter().getCount() - 2
+                        && list.getLastVisiblePosition() == list.getAdapter().getCount() - 1
                         && list.getChildAt(list.getChildCount() - 1).getBottom() >= list.getHeight()) {
                     long time = System.currentTimeMillis();
                     if ((time - lastScrollDate) > 200
