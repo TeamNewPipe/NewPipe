1170c508b4b947ea01a91737664f09b9fe68e1d0
Christian Schabesberger <chris.schabesberger@mailbox.org>
2016-08-02 01:26:12 +0200

    add new architecture

---
+++ app/src/main/java/org/schabi/newpipe/MainActivity.java
@@ -0,0 +1,13 @@
+package org.schabi.newpipe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
