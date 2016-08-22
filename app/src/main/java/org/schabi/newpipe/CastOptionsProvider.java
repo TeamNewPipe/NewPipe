68c06fdcbaf9bd8d138b6b937f6982abb35c85e3
Robert-Jan Keizer <robertjankeizer@gmail.com>
2016-08-23 00:47:00 +0200

    Updated chromecast stream notification

--- app/src/main/java/org/schabi/newpipe/CastOptionsProvider.java
+++ app/src/main/java/org/schabi/newpipe/CastOptionsProvider.java
@@ -2,23 +2,61 @@ package org.schabi.newpipe;
 
 import android.content.Context;
 
+import com.google.android.gms.cast.MediaMetadata;
 import com.google.android.gms.cast.framework.CastOptions;
 import com.google.android.gms.cast.framework.OptionsProvider;
 import com.google.android.gms.cast.framework.SessionProvider;
+import com.google.android.gms.cast.framework.media.CastMediaOptions;
+import com.google.android.gms.cast.framework.media.ImagePicker;
+import com.google.android.gms.cast.framework.media.MediaIntentReceiver;
+import com.google.android.gms.cast.framework.media.NotificationOptions;
+import com.google.android.gms.common.images.WebImage;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class CastOptionsProvider implements OptionsProvider {
 
     @Override
     public CastOptions getCastOptions(Context context) {
+
+        NotificationOptions notificationOptions = new NotificationOptions.Builder()
+                .setActions(Arrays.asList(MediaIntentReceiver.ACTION_TOGGLE_PLAYBACK, MediaIntentReceiver.ACTION_STOP_CASTING), new int[]{1, 2})
+                .build();
+
+        CastMediaOptions mediaOptions = new CastMediaOptions.Builder()
+                .setNotificationOptions(notificationOptions)
+                .setImagePicker(new ImagePickerImpl())
+                .build();
+
         return new CastOptions.Builder()
                 .setReceiverApplicationId(context.getString(R.string.chromecast_app_id))
+                .setCastMediaOptions(mediaOptions)
                 .build();
     }
 
     @Override
     public List<SessionProvider> getAdditionalSessionProviders(Context context) {
         return null;
+    }
+
+    private static class ImagePickerImpl extends ImagePicker {
+
+        @Override
+        public WebImage onPickImage(MediaMetadata mediaMetadata, int type) {
+            if ((mediaMetadata == null) || !mediaMetadata.hasImages()) {
+                return null;
+            }
+            List<WebImage> images = mediaMetadata.getImages();
+            if (images.size() == 1) {
+                return images.get(0);
+            } else {
+                if (type == ImagePicker.IMAGE_TYPE_MEDIA_ROUTE_CONTROLLER_DIALOG_BACKGROUND) {
+                    return images.get(0);
+                } else {
+                    return images.get(1);
+                }
+            }
+        }
     }
 }
