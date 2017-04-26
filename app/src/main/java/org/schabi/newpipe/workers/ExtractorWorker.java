a4b61bf73092394f4bc2d9894a83aa881a1deb95
Mauricio Colli <mauriciocolli@outlook.com>
2017-04-26 16:25:09 -0300

    Add new workers

--- app/src/main/java/org/schabi/newpipe/workers/ExtractorWorker.java
+++ app/src/main/java/org/schabi/newpipe/workers/ExtractorWorker.java
@@ -2,18 +2,12 @@ package org.schabi.newpipe.workers;
 
 import android.app.Activity;
 import android.content.Context;
-import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 
-import org.schabi.newpipe.extractor.NewPipe;
-import org.schabi.newpipe.extractor.StreamingService;
-import org.schabi.newpipe.extractor.stream_info.StreamInfo;
 import org.schabi.newpipe.report.ErrorActivity;
 
-import java.io.InterruptedIOException;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Common properties of ExtractorWorkers
@@ -21,38 +15,18 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * @author mauriciocolli
  */
 @SuppressWarnings("WeakerAccess")
-public abstract class ExtractorWorker extends Thread {
-
-    private final AtomicBoolean isRunning = new AtomicBoolean(false);
-
+public abstract class ExtractorWorker extends AbstractWorker {
     private final String url;
-    private final int serviceId;
-    private Context context;
-    private Handler handler;
-    private StreamingService service;
 
     public ExtractorWorker(Context context, String url, int serviceId) {
-        this.context = context;
+        super(context, serviceId);
         this.url = url;
-        this.serviceId = serviceId;
-        this.handler = new Handler(context.getMainLooper());
         if (url.length() >= 40) setName("Thread-" + url.substring(url.length() - 11, url.length()));
     }
 
     @Override
-    public void run() {
-        try {
-            isRunning.set(true);
-            service = NewPipe.getService(serviceId);
-            doWork(serviceId, url);
-        } catch (Exception e) {
-            // Handle the exception only if thread is not interrupted
-            if (!isInterrupted() && !(e instanceof InterruptedIOException) && !(e.getCause() instanceof InterruptedIOException)) {
-                handleException(e, serviceId, url);
-            }
-        } finally {
-            isRunning.set(false);
-        }
+    protected void doWork(int serviceId) throws Exception {
+        doWork(serviceId, url);
     }
 
     /**
@@ -65,6 +39,10 @@ public abstract class ExtractorWorker extends Thread {
      */
     protected abstract void doWork(int serviceId, String url) throws Exception;
 
+    @Override
+    protected void handleException(Exception exception, int serviceId) {
+        handleException(exception, serviceId, url);
+    }
 
     /**
      * Method that handle the exception thrown by the {@link #doWork(int, String)}.
@@ -99,63 +77,12 @@ public abstract class ExtractorWorker extends Thread {
         }
 
         if (getContext() instanceof Activity) {
-            View rootView = getContext() != null ? ((Activity) getContext()).findViewById(android.R.id.content) : null;
+            View rootView = getContext() instanceof Activity ? ((Activity) getContext()).findViewById(android.R.id.content) : null;
             ErrorActivity.reportError(getHandler(), getContext(), errorsList, null, rootView, ErrorActivity.ErrorInfo.make(errorUserAction, getServiceName(), url, 0 /* no message for the user */));
         }
     }
 
-    /**
-     * Return true if the extraction is not completed yet
-     *
-     * @return the value of the AtomicBoolean {@link #isRunning}
-     */
-    public boolean isRunning() {
-        return isRunning.get();
-    }
-
-    /**
-     * Cancel this ExtractorWorker, calling {@link #onDestroy()} and interrupting this thread.
-     * <p>
-     * <b>Note:</b> Any I/O that is active in the moment that this method is called will be canceled and a Exception will be thrown, because of the {@link #interrupt()}.<br>
-     * This is useful when you don't want the resulting {@link StreamInfo} anymore, but don't want to waste bandwidth, otherwise it'd run till it receives the StreamInfo.
-     */
-    public void cancel() {
-        onDestroy();
-        this.interrupt();
-    }
-
-    /**
-     * Method that discards everything that doesn't need anymore.<br>
-     * Subclasses can override this method to destroy their garbage.
-     */
-    protected void onDestroy() {
-        this.isRunning.set(false);
-        this.context = null;
-        this.handler = null;
-        this.service = null;
-    }
-
-    public Handler getHandler() {
-        return handler;
-    }
-
     public String getUrl() {
         return url;
-    }
-
-    public StreamingService getService() {
-        return service;
-    }
-
-    public int getServiceId() {
-        return serviceId;
-    }
-
-    public String getServiceName() {
-        return service == null ? "none" : service.getServiceInfo().name;
-    }
-
-    public Context getContext() {
-        return context;
     }
 }
