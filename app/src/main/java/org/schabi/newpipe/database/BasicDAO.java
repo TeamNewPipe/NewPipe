10c4f7b465f37020ab8aab43a040fb60439cf239
Tonelico <karyogamy@users.noreply.github.com>
2017-08-07 06:02:30 -0700

    Added basic channel subscription and feed pages (#620)
    Added basic channel subscription and feed pages
    
    - Room Persistence for sqlite support.
    - RxJava2 for reactive async support.
    - Stetho for database inspection support.
    - Enabled Multidex for debug build.

---
+++ app/src/main/java/org/schabi/newpipe/database/BasicDAO.java
@@ -0,0 +1,48 @@
+package org.schabi.newpipe.database;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Update;
+
+import java.util.Collection;
+import java.util.List;
+
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
+
+@Dao
+public interface BasicDAO<Entity> {
+    /* Inserts */
+    @Insert(onConflict = OnConflictStrategy.FAIL)
+    long insert(final Entity entity);
+
+    @Insert(onConflict = OnConflictStrategy.FAIL)
+    List<Long> insertAll(final Entity... entities);
+
+    @Insert(onConflict = OnConflictStrategy.FAIL)
+    List<Long> insertAll(final Collection<Entity> entities);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    long upsert(final Entity entity);
+
+    /* Searches */
+    Flowable<List<Entity>> findAll();
+
+    Flowable<List<Entity>> listByService(int serviceId);
+
+    /* Deletes */
+    @Delete
+    int delete(final Entity entity);
+
+    @Delete
+    int delete(final Collection<Entity> entities);
+
+    /* Updates */
+    @Update
+    int update(final Entity entity);
+
+    @Update
+    int update(final Collection<Entity> entities);
+}
