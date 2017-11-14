98e617001dd63200003041bcacfcdb095c42fb11
John Zhen Mo <zhenmogukl@gmail.com>
2017-11-14 10:51:44 -0800

    -Added search suggestion insert per #840.

--- app/src/main/java/org/schabi/newpipe/fragments/list/search/SearchFragment.java
+++ app/src/main/java/org/schabi/newpipe/fragments/list/search/SearchFragment.java
@@ -165,7 +165,7 @@ public class SearchFragment extends BaseListFragment<SearchResult, ListExtractor
         suggestionListAdapter = new SuggestionListAdapter(activity);
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(activity);
         isSearchHistoryEnabled = preferences.getBoolean(getString(R.string.enable_search_history_key), true);
-        suggestionListAdapter.setShowSugestinHistory(isSearchHistoryEnabled);
+        suggestionListAdapter.setShowSuggestionHistory(isSearchHistoryEnabled);
         
         searchHistoryDAO = NewPipeDatabase.getInstance().searchHistoryDAO();
     }
@@ -443,6 +443,11 @@ public class SearchFragment extends BaseListFragment<SearchResult, ListExtractor
             @Override
             public void onSuggestionItemSelected(SuggestionItem item) {
                 search(item.query);
+                searchEditText.setText(item.query);
+            }
+
+            @Override
+            public void onSuggestionItemInserted(SuggestionItem item) {
                 searchEditText.setText(item.query);
             }
 
