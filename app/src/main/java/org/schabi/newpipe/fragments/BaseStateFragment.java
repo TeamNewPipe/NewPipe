bc7332780de81f720760d8fe5ff5ed2011e0a5a0
Christian Schabesberger <chris.schabesberger@mailbox.org>
2017-12-17 12:55:30 +0100

    fix show rending before page was loaded

--- app/src/main/java/org/schabi/newpipe/fragments/BaseStateFragment.java
+++ app/src/main/java/org/schabi/newpipe/fragments/BaseStateFragment.java
@@ -219,6 +219,7 @@ public abstract class BaseStateFragment<I> extends BaseFragment implements ViewC
 
         if (serviceName == null) serviceName = "none";
         if (request == null) request = "none";
+
         ErrorActivity.reportError(getContext(), exception, MainActivity.class, null, ErrorActivity.ErrorInfo.make(userAction, serviceName, request, errorId));
     }
 
