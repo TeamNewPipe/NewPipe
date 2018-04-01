2837e44bab24ddd2be53578958d097add0f4fa44
shan11812 <11812r@gmail.com>
2018-04-01 20:47:24 +0530

    Adds 'Add to Playlist' option to Related and Channel lists

--- app/src/main/java/org/schabi/newpipe/fragments/detail/VideoDetailFragment.java
+++ app/src/main/java/org/schabi/newpipe/fragments/detail/VideoDetailFragment.java
@@ -86,6 +86,7 @@ import org.schabi.newpipe.util.ThemeHelper;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.LinkedList;
 
 import icepick.State;
@@ -549,7 +550,8 @@ public class VideoDetailFragment
 
         final String[] commands = new String[]{
                 context.getResources().getString(R.string.enqueue_on_background),
-                context.getResources().getString(R.string.enqueue_on_popup)
+                context.getResources().getString(R.string.enqueue_on_popup),
+                context.getResources().getString(R.string.append_playlist)
         };
 
         final DialogInterface.OnClickListener actions = (DialogInterface dialogInterface, int i) -> {
@@ -559,6 +561,12 @@ public class VideoDetailFragment
                     break;
                 case 1:
                     NavigationHelper.enqueueOnPopupPlayer(getActivity(), new SinglePlayQueue(item));
+                    break;
+                case 2:
+                    if (getFragmentManager() != null) {
+                        PlaylistAppendDialog.fromStreamInfoItems(Collections.singletonList(item))
+                                .show(getFragmentManager(), TAG);
+                    }
                     break;
                 default:
                     break;
