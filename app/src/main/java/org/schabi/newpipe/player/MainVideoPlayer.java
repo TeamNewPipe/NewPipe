43b6ee3c13c8275094bd80edf2ef181a79982255
Jeffrey Liu <scubajeffrey@gmail.com>
2018-04-23 14:48:22 +0800

    Revert "Set SYSTEM_UI_FLAG_LOW_PROFILE instead of SYSTEM_UI_FLAG_HIDE_NAVIGATION in full screen mode"
    This reverts commit 6133471c3abbbca2455dc67c1efe3aeb4e4435fe.

--- app/src/main/java/org/schabi/newpipe/player/MainVideoPlayer.java
+++ app/src/main/java/org/schabi/newpipe/player/MainVideoPlayer.java
@@ -259,8 +259,9 @@ public final class MainVideoPlayer extends AppCompatActivity
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
             int visibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LOW_PROFILE;
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
                 visibility |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
             }
