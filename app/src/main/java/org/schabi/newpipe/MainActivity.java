c78cc6f2fdd7e8c8cd25337a06d33560954d5e3b
TobiGr <tobigr@users.noreply.github.com>
2018-05-24 17:10:28 +0200

    Add dialog to accept privacy policy before sending crash report
    Add link to privacy policy in about fragment
    Replace some onClickListeners with Lamdas

--- app/src/main/java/org/schabi/newpipe/MainActivity.java
+++ app/src/main/java/org/schabi/newpipe/MainActivity.java
@@ -20,6 +20,9 @@
 
 package org.schabi.newpipe;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
