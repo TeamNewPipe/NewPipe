429dddc6c970b7b583e051e5e1227a1b5cb9119e
Kartikey Kushwaha <kartikey92@gmail.com>
2018-06-09 01:13:37 +0530

    reintroduced hideControls method.

--- app/src/main/java/org/schabi/newpipe/player/PopupVideoPlayer.java
+++ app/src/main/java/org/schabi/newpipe/player/PopupVideoPlayer.java
@@ -662,6 +662,8 @@ public final class PopupVideoPlayer extends Service {
             videoPlayPause.setBackgroundResource(R.drawable.ic_pause_white);
             lockManager.acquireWifiAndCpu();
 
+            hideControls(DEFAULT_CONTROLS_DURATION, DEFAULT_CONTROLS_HIDE_TIME);
+
             windowLayoutParams.flags = WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON;
             windowManager.updateViewLayout(playerImpl.getRootView(), windowLayoutParams);
         }
