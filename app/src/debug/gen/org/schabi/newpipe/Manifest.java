2d5bc3ada85d2fb0151bfe32b6a6fef9d08c0df8
Kartikey Kushwaha <krtkush@users.noreply.github.com>
2018-10-18 23:02:30 +0530

    Delete Manifest.java

--- app/src/debug/gen/org/schabi/newpipe/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package org.schabi.newpipe;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
