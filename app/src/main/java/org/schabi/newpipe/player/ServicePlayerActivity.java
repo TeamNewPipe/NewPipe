7f246b2d3de9dbfa4cb5791cf6dfea6c2b2d7ad3
Robin <robin.gawenda@ondamedia.de>
2019-03-05 19:54:37 +0100

    Removed unused import

--- app/src/main/java/org/schabi/newpipe/player/ServicePlayerActivity.java
+++ app/src/main/java/org/schabi/newpipe/player/ServicePlayerActivity.java
@@ -3,7 +3,6 @@ package org.schabi.newpipe.player;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.provider.Settings;
