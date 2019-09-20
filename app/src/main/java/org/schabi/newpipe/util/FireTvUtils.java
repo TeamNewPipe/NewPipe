6791de5fc0325bf4fd1fa7bc2b18f96930887783
Alexander <eltex.developers@gmail.com>
2019-09-20 16:23:17 +0700

    Do not discriminate against non-Amazon TV boxes

--- app/src/main/java/org/schabi/newpipe/util/FireTvUtils.java
+++ app/src/main/java/org/schabi/newpipe/util/FireTvUtils.java
@@ -1,10 +1,18 @@
 package org.schabi.newpipe.util;
 
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+
 import org.schabi.newpipe.App;
 
 public class FireTvUtils {
+    @SuppressLint("InlinedApi")
     public static boolean isFireTv(){
         final String AMAZON_FEATURE_FIRE_TV = "amazon.hardware.fire_tv";
-        return App.getApp().getPackageManager().hasSystemFeature(AMAZON_FEATURE_FIRE_TV);
+
+        PackageManager pm =  App.getApp().getPackageManager();
+
+        return pm.hasSystemFeature(AMAZON_FEATURE_FIRE_TV)
+                || pm.hasSystemFeature(PackageManager.FEATURE_LEANBACK);
     }
 }
