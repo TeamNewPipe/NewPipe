5e824dd2a1a5d9b17d3f0c1d733acfead9060084
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2019-10-21 21:25:00 +0200

    Adjust ShareUtils to use the custom website if activated.

--- app/src/main/java/org/schabi/newpipe/util/ShareUtils.java
+++ app/src/main/java/org/schabi/newpipe/util/ShareUtils.java
@@ -2,13 +2,19 @@ package org.schabi.newpipe.util;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 
+import android.preference.PreferenceManager;
 import org.schabi.newpipe.R;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class ShareUtils {
     public static void openUrlInBrowser(Context context, String url) {
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        String shareUrl = getShareUrl(PreferenceManager.getDefaultSharedPreferences(context), url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(shareUrl));
         context.startActivity(Intent.createChooser(intent, context.getString(R.string.share_dialog_title)));
     }
 
@@ -16,7 +22,24 @@ public class ShareUtils {
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("text/plain");
         intent.putExtra(Intent.EXTRA_SUBJECT, subject);
-        intent.putExtra(Intent.EXTRA_TEXT, url);
+        String shareUrl = getShareUrl(PreferenceManager.getDefaultSharedPreferences(context), url);
+        intent.putExtra(Intent.EXTRA_TEXT, shareUrl);
         context.startActivity(Intent.createChooser(intent, context.getString(R.string.share_dialog_title)));
+    }
+
+    private static String getShareUrl(SharedPreferences prefs, String url) {
+        if (prefs.getBoolean("use_custom_share_website", false)) {
+            return prefs.getString("custom_share_website", "https://youtube.com")
+                + extractWatchParameter(url);
+        } else {
+            return url;
+        }
+    }
+
+    private static String extractWatchParameter(String url) {
+        Pattern pattern = Pattern.compile("(watch\\?v=[^&]+)");
+        Matcher matcher = pattern.matcher(url);
+        matcher.find();
+        return matcher.group(1);
     }
 }
