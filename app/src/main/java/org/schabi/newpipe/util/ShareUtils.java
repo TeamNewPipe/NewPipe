c20e73b37bf94323574dd64b0bfdcddc4ffca415
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2019-10-23 21:07:59 +0200

    Use URL class to parse path and query instead of own regex.

--- app/src/main/java/org/schabi/newpipe/util/ShareUtils.java
+++ app/src/main/java/org/schabi/newpipe/util/ShareUtils.java
@@ -8,38 +8,46 @@ import android.net.Uri;
 import android.preference.PreferenceManager;
 import org.schabi.newpipe.R;
 
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class ShareUtils {
     public static void openUrlInBrowser(Context context, String url) {
-        String shareUrl = getShareUrl(PreferenceManager.getDefaultSharedPreferences(context), url);
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(shareUrl));
-        context.startActivity(Intent.createChooser(intent, context.getString(R.string.share_dialog_title)));
-    }
+        try {
+            String shareUrl = getShareUrl(PreferenceManager.getDefaultSharedPreferences(context), url);
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(shareUrl));
+            context.startActivity(Intent.createChooser(intent, context.getString(R.string.share_dialog_title)));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        }
 
     public static void shareUrl(Context context, String subject, String url) {
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("text/plain");
-        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
-        String shareUrl = getShareUrl(PreferenceManager.getDefaultSharedPreferences(context), url);
-        intent.putExtra(Intent.EXTRA_TEXT, shareUrl);
-        context.startActivity(Intent.createChooser(intent, context.getString(R.string.share_dialog_title)));
+        try {
+            String shareUrl = getShareUrl(PreferenceManager.getDefaultSharedPreferences(context), url);
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+            intent.putExtra(Intent.EXTRA_TEXT, shareUrl);
+            context.startActivity(Intent.createChooser(intent, context.getString(R.string.share_dialog_title)));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    private static String getShareUrl(SharedPreferences prefs, String url) {
+    private static String getShareUrl(SharedPreferences prefs, String url)
+        throws MalformedURLException {
         if (prefs.getBoolean("use_custom_share_website", false)) {
             return prefs.getString("custom_share_website", "https://youtube.com")
-                + "/" + extractWatchParameter(url);
+                + extractPathAndQuery(url);
         } else {
             return url;
         }
     }
 
-    private static String extractWatchParameter(String url) {
-        Pattern pattern = Pattern.compile("(watch\\?v=[^&]+)");
-        Matcher matcher = pattern.matcher(url);
-        matcher.find();
-        return matcher.group(1);
+    private static String extractPathAndQuery(String link) throws MalformedURLException {
+        URL url = new URL(link);
+       return url.getPath() + "?" + url.getQuery();
     }
 }
