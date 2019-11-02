1096b63d6b5a62b7f2e60ba7b152f0de60f6b8dd
Vasiliy <nvasya95@gmail.com>
2019-11-02 07:52:28 +0200

    Adjust code style

--- app/src/main/java/org/schabi/newpipe/util/ShortcutsHelper.java
+++ app/src/main/java/org/schabi/newpipe/util/ShortcutsHelper.java
@@ -41,145 +41,145 @@ import okhttp3.Response;
 
 public final class ShortcutsHelper {
 
-	public static final String ACTION_OPEN_SHORTCUT = "org.schabi.newpipe.action.OPEN_SHORTCUT";
+    public static final String ACTION_OPEN_SHORTCUT = "org.schabi.newpipe.action.OPEN_SHORTCUT";
 
-	private ShortcutsHelper() {
-	}
+    private ShortcutsHelper() {
+    }
 
-	@SuppressLint("CheckResult")
-	@SuppressWarnings("ResultOfMethodCallIgnored")
-	public static void addShortcut(@Nullable final Context context, @NonNull final ChannelInfoItem data) {
-		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N_MR1 || context == null) {
-			return;
-		}
-		final ShortcutManager manager = (ShortcutManager) context.getSystemService(Context.SHORTCUT_SERVICE);
-		if (manager == null) {
-			return;
-		}
-		Single.fromCallable(() -> getIcon(context, data.getThumbnailUrl(), manager.getIconMaxWidth(),
-				manager.getIconMaxHeight(), R.drawable.ic_newpipe_triangle_white))
-				.subscribeOn(Schedulers.computation())
-				.map(icon -> new ShortcutInfo.Builder(context, getShortcutId(data))
-						.setShortLabel(data.getName())
-						.setLongLabel(data.getName())
-						.setIcon(icon.toIcon())
-						.setIntent(createIntent(context, data)))
-				.observeOn(AndroidSchedulers.mainThread())
-				.subscribe(builder -> {
-					final String id = getShortcutId(data);
-					final List<ShortcutInfo> shortcuts = manager.getDynamicShortcuts();
-					final int limit = manager.getMaxShortcutCountPerActivity();
-					for (int i = 0; i < shortcuts.size(); i++) {
-						final ShortcutInfo shortcut = shortcuts.get(i);
-						if (id.equals(shortcut.getId())) {
-							// if shortcut already exists - increase rank and update it
-							builder.setRank(shortcut.getRank() + 1);
-							manager.updateShortcuts(Collections.singletonList(builder.build()));
-							return;
-						}
-					}
-					builder.setRank(1);
-					if (!shortcuts.isEmpty() && shortcuts.size() >= limit) {
-						// we will get an exception if shortcuts count exceed the limit
-						manager.removeDynamicShortcuts(Collections.singletonList(shortcuts.get(shortcuts.size() - 1).getId()));
-					}
-					manager.addDynamicShortcuts(Collections.singletonList(builder.build()));
-				}, e -> {
-					if (BuildConfig.DEBUG) {
-						e.printStackTrace();
-					}
-				});
-	}
+    @SuppressLint("CheckResult")
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    public static void addShortcut(@Nullable final Context context, @NonNull final ChannelInfoItem data) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N_MR1 || context == null) {
+            return;
+        }
+        final ShortcutManager manager = (ShortcutManager) context.getSystemService(Context.SHORTCUT_SERVICE);
+        if (manager == null) {
+            return;
+        }
+        Single.fromCallable(() -> getIcon(context, data.getThumbnailUrl(), manager.getIconMaxWidth(),
+                manager.getIconMaxHeight(), R.drawable.ic_newpipe_triangle_white))
+                .subscribeOn(Schedulers.computation())
+                .map(icon -> new ShortcutInfo.Builder(context, getShortcutId(data))
+                        .setShortLabel(data.getName())
+                        .setLongLabel(data.getName())
+                        .setIcon(icon.toIcon())
+                        .setIntent(createIntent(context, data)))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(builder -> {
+                    final String id = getShortcutId(data);
+                    final List<ShortcutInfo> shortcuts = manager.getDynamicShortcuts();
+                    final int limit = manager.getMaxShortcutCountPerActivity();
+                    for (int i = 0; i < shortcuts.size(); i++) {
+                        final ShortcutInfo shortcut = shortcuts.get(i);
+                        if (id.equals(shortcut.getId())) {
+                            // if shortcut already exists - increase rank and update it
+                            builder.setRank(shortcut.getRank() + 1);
+                            manager.updateShortcuts(Collections.singletonList(builder.build()));
+                            return;
+                        }
+                    }
+                    builder.setRank(1);
+                    if (!shortcuts.isEmpty() && shortcuts.size() >= limit) {
+                        // we will get an exception if shortcuts count exceed the limit
+                        manager.removeDynamicShortcuts(Collections.singletonList(shortcuts.get(shortcuts.size() - 1).getId()));
+                    }
+                    manager.addDynamicShortcuts(Collections.singletonList(builder.build()));
+                }, e -> {
+                    if (BuildConfig.DEBUG) {
+                        e.printStackTrace();
+                    }
+                });
+    }
 
-	@SuppressLint("CheckResult")
-	@SuppressWarnings("ResultOfMethodCallIgnored")
-	public static void pinShortcut(@Nullable final Context context, @NonNull final ChannelInfoItem data) {
-		if (context == null) {
-			return;
-		}
-		final ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-		if (am == null) {
-			return;
-		}
-		final int iconSize = am.getLauncherLargeIconSize();
-		Single.fromCallable(() -> getIcon(context, data.getThumbnailUrl(), iconSize, iconSize, R.mipmap.ic_launcher))
-				.subscribeOn(Schedulers.computation())
-				.map(icon -> new ShortcutInfoCompat.Builder(context, getShortcutId(data))
-						.setShortLabel(data.getName())
-						.setLongLabel(data.getName())
-						.setIcon(icon)
-						.setIntent(createIntent(context, data)))
-				.observeOn(AndroidSchedulers.mainThread())
-				.subscribe(builder -> ShortcutManagerCompat.requestPinShortcut(context, builder.build(), null), e -> {
-					if (BuildConfig.DEBUG) {
-						e.printStackTrace();
-					}
-				});
-	}
+    @SuppressLint("CheckResult")
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    public static void pinShortcut(@Nullable final Context context, @NonNull final ChannelInfoItem data) {
+        if (context == null) {
+            return;
+        }
+        final ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        if (am == null) {
+            return;
+        }
+        final int iconSize = am.getLauncherLargeIconSize();
+        Single.fromCallable(() -> getIcon(context, data.getThumbnailUrl(), iconSize, iconSize, R.mipmap.ic_launcher))
+                .subscribeOn(Schedulers.computation())
+                .map(icon -> new ShortcutInfoCompat.Builder(context, getShortcutId(data))
+                        .setShortLabel(data.getName())
+                        .setLongLabel(data.getName())
+                        .setIcon(icon)
+                        .setIntent(createIntent(context, data)))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(builder -> ShortcutManagerCompat.requestPinShortcut(context, builder.build(), null), e -> {
+                    if (BuildConfig.DEBUG) {
+                        e.printStackTrace();
+                    }
+                });
+    }
 
-	@NonNull
-	private static Intent createIntent(@NonNull Context context, @NonNull ChannelInfoItem channel) {
-		final Intent intent = new Intent(context, MainActivity.class);
-		intent.setAction(ACTION_OPEN_SHORTCUT);
-		intent.putExtra(Constants.KEY_URL, channel.getUrl());
-		intent.putExtra(Constants.KEY_SERVICE_ID, channel.getServiceId());
-		intent.putExtra(Constants.KEY_TITLE, channel.getName());
-		return intent;
-	}
+    @NonNull
+    private static Intent createIntent(@NonNull Context context, @NonNull ChannelInfoItem channel) {
+        final Intent intent = new Intent(context, MainActivity.class);
+        intent.setAction(ACTION_OPEN_SHORTCUT);
+        intent.putExtra(Constants.KEY_URL, channel.getUrl());
+        intent.putExtra(Constants.KEY_SERVICE_ID, channel.getServiceId());
+        intent.putExtra(Constants.KEY_TITLE, channel.getName());
+        return intent;
+    }
 
-	@NonNull
-	private static String getShortcutId(@NonNull ChannelInfoItem channel) {
-		return "s_" + channel.getUrl().hashCode();
-	}
+    @NonNull
+    private static String getShortcutId(@NonNull ChannelInfoItem channel) {
+        return "s_" + channel.getUrl().hashCode();
+    }
 
-	@NonNull
-	private static IconCompat getIcon(@NonNull Context context, final String url, int width,
-									  int height, @DrawableRes int defaultIcon) {
-		Bitmap bitmap = null;
-		try {
-			final OkHttpClient client = new OkHttpClient();
-			final Request request = new Request.Builder()
-					.url(url)
-					.get()
-					.build();
-			final Response response = client.newCall(request).execute();
-			if (response.isSuccessful()) {
-				final InputStream inputStream = response.body().byteStream();
-				bitmap = BitmapFactory.decodeStream(inputStream);
+    @NonNull
+    private static IconCompat getIcon(@NonNull Context context, final String url, int width,
+                                      int height, @DrawableRes int defaultIcon) {
+        Bitmap bitmap = null;
+        try {
+            final OkHttpClient client = new OkHttpClient();
+            final Request request = new Request.Builder()
+                    .url(url)
+                    .get()
+                    .build();
+            final Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                final InputStream inputStream = response.body().byteStream();
+                bitmap = BitmapFactory.decodeStream(inputStream);
 
-				final Bitmap thumb = ThumbnailUtils.extractThumbnail(bitmap, width, height);
-				final IconCompat icon = IconCompat.createWithBitmap(createCircleBitmap(thumb));
-				thumb.recycle();
-				return icon;
-			} else {
-				return IconCompat.createWithResource(context, defaultIcon);
-			}
-		} catch (Exception e) {
-			return IconCompat.createWithResource(context, defaultIcon);
-		} finally {
-			if (bitmap != null) {
-				bitmap.recycle();
-			}
-		}
-	}
+                final Bitmap thumb = ThumbnailUtils.extractThumbnail(bitmap, width, height);
+                final IconCompat icon = IconCompat.createWithBitmap(createCircleBitmap(thumb));
+                thumb.recycle();
+                return icon;
+            } else {
+                return IconCompat.createWithResource(context, defaultIcon);
+            }
+        } catch (Exception e) {
+            return IconCompat.createWithResource(context, defaultIcon);
+        } finally {
+            if (bitmap != null) {
+                bitmap.recycle();
+            }
+        }
+    }
 
-	@NonNull
-	private static Bitmap createCircleBitmap(@NonNull final Bitmap bitmap) {
-		final Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
-				bitmap.getHeight(), Bitmap.Config.ARGB_8888);
-		final Canvas canvas = new Canvas(output);
+    @NonNull
+    private static Bitmap createCircleBitmap(@NonNull final Bitmap bitmap) {
+        final Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        final Canvas canvas = new Canvas(output);
 
-		final int color = 0xff424242;
-		final Paint paint = new Paint();
-		final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
 
-		paint.setAntiAlias(true);
-		canvas.drawARGB(0, 0, 0, 0);
-		paint.setColor(color);
-		canvas.drawCircle(bitmap.getWidth() / 2, bitmap.getHeight() / 2,
-				bitmap.getWidth() / 2, paint);
-		paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-		canvas.drawBitmap(bitmap, rect, rect, paint);
-		return output;
-	}
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawCircle(bitmap.getWidth() / 2, bitmap.getHeight() / 2,
+                bitmap.getWidth() / 2, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
+    }
 }
