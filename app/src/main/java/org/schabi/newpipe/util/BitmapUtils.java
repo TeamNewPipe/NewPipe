cf13f5ca56950944f5ab44e8c00c8c7566e55ed2
k1rakishou <kez1911@protonmail.com>
2019-11-08 20:47:42 +0300

    Rebase onto the latest dev, update appcompat dependencies to use androidx

--- app/src/main/java/org/schabi/newpipe/util/BitmapUtils.java
+++ app/src/main/java/org/schabi/newpipe/util/BitmapUtils.java
@@ -1,7 +1,8 @@
 package org.schabi.newpipe.util;
 
 import android.graphics.Bitmap;
-import android.support.annotation.Nullable;
+
+import androidx.annotation.Nullable;
 
 public class BitmapUtils {
 
