d58433e961ef01827633f77e6f5b763a8e6d29b1
Markus Richter <8398165+mqus@users.noreply.github.com>
2019-12-28 23:47:58 +0100

    remove TLS 1.1 support for kitkat

--- app/src/main/java/org/schabi/newpipe/util/TLSSocketFactoryCompat.java
+++ app/src/main/java/org/schabi/newpipe/util/TLSSocketFactoryCompat.java
@@ -97,7 +97,7 @@ public class TLSSocketFactoryCompat extends SSLSocketFactory {
 
     private Socket enableTLSOnSocket(Socket socket) {
         if (socket != null && (socket instanceof SSLSocket)) {
-            ((SSLSocket) socket).setEnabledProtocols(new String[]{"TLSv1.1", "TLSv1.2"});
+            ((SSLSocket) socket).setEnabledProtocols(new String[]{"TLSv1.2"});
         }
         return socket;
     }
