1f24c186146c17f6fe5e140539cc7e56b2266062
Harshal Lele <harshallele97@gmail.com>
2020-01-28 20:14:35 +0530

    reformatted and commented code

--- app/src/main/java/org/schabi/newpipe/settings/VideoAudioSettingsFragment.java
+++ app/src/main/java/org/schabi/newpipe/settings/VideoAudioSettingsFragment.java
@@ -24,11 +24,18 @@ public class VideoAudioSettingsFragment extends BasePreferenceFragment {
         listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
             @Override
             public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+
+                //on M and above, if user chooses to minimise to popup player on exit and the app doesn't have
+                //display over other apps permission, show a snackbar to let the user give permission
                 if(s.equals(getString(R.string.minimize_on_exit_key))){
+
                     String newSetting = sharedPreferences.getString(s,null);
                     if(newSetting != null){
+
                         if(newSetting.equals(getString(R.string.minimize_on_exit_popup_key))){
+
                             if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(getContext())){
+
                                 Snackbar.make(getListView(),R.string.permission_display_over_apps,Snackbar.LENGTH_INDEFINITE)
                                         .setAction(R.string.settings, new View.OnClickListener() {
                                             @Override
