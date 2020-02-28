6ea4ee30f4357346e6135fb7cd74f644308941be
Stypox <stypox@pm.me>
2020-02-28 15:10:29 +0100

    Update app/src/main/java/org/schabi/newpipe/util/FireTvUtils.java

--- app/src/main/java/org/schabi/newpipe/util/FireTvUtils.java
+++ app/src/main/java/org/schabi/newpipe/util/FireTvUtils.java
@@ -2,8 +2,6 @@ package org.schabi.newpipe.util;
 
 import android.app.Application;
 
-import org.schabi.newpipe.App;
-
 public class FireTvUtils {
     public static boolean isFireTv(Application application){
         final String AMAZON_FEATURE_FIRE_TV = "amazon.hardware.fire_tv";
