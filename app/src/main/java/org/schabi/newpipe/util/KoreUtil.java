9ca6cfd6374159b50ccc4716bf8d3ba15891014d
Stypox <stypox@pm.me>
2020-05-06 20:40:44 +0200

    Fix Kodi button showing up in unsupported services

--- app/src/main/java/org/schabi/newpipe/util/KoreUtil.java
+++ app/src/main/java/org/schabi/newpipe/util/KoreUtil.java
@@ -7,9 +7,15 @@ import android.content.DialogInterface;
 import androidx.appcompat.app.AlertDialog;
 
 import org.schabi.newpipe.R;
+import org.schabi.newpipe.extractor.ServiceList;
 
 public final class KoreUtil {
     private KoreUtil() { }
+
+    public static boolean isServiceSupportedByKore(final int serviceId) {
+        return (serviceId == ServiceList.YouTube.getServiceId()
+                || serviceId == ServiceList.SoundCloud.getServiceId());
+    }
 
     public static void showInstallKoreDialog(final Context context) {
         final AlertDialog.Builder builder = new AlertDialog.Builder(context);
