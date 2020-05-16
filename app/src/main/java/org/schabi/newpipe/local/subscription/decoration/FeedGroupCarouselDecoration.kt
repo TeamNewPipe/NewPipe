28b0b126633f725433f5e6fdbe67096d1b448999
Roy Yosef <royosef10@gmail.com>
2020-05-16 19:17:19 +0300

    fix(report-error&feed-group-carousel)
    * fix info section direction in report error page
    * fix margin at the feed group carousel in subscriptions page

--- app/src/main/java/org/schabi/newpipe/local/subscription/decoration/FeedGroupCarouselDecoration.kt
+++ app/src/main/java/org/schabi/newpipe/local/subscription/decoration/FeedGroupCarouselDecoration.kt
@@ -5,12 +5,14 @@ import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import org.schabi.newpipe.R
+import org.schabi.newpipe.util.LocalizeLayoutUtils
 
 class FeedGroupCarouselDecoration(context: Context) : RecyclerView.ItemDecoration() {
 
     private val marginStartEnd: Int
     private val marginTopBottom: Int
     private val marginBetweenItems: Int
+    private val isRTL: Boolean
 
     init {
         with(context.resources) {
@@ -18,18 +20,32 @@ class FeedGroupCarouselDecoration(context: Context) : RecyclerView.ItemDecoratio
             marginTopBottom = getDimensionPixelOffset(R.dimen.feed_group_carousel_top_bottom_margin)
             marginBetweenItems = getDimensionPixelOffset(R.dimen.feed_group_carousel_between_items_margin)
         }
+
+        isRTL = LocalizeLayoutUtils.isRTL(context)
     }
 
     override fun getItemOffsets(outRect: Rect, child: View, parent: RecyclerView, state: RecyclerView.State) {
         val childAdapterPosition = parent.getChildAdapterPosition(child)
         val childAdapterCount = parent.adapter?.itemCount ?: 0
 
-        outRect.set(marginBetweenItems, marginTopBottom, 0, marginTopBottom)
+        outRect.set(0, marginTopBottom, 0, marginTopBottom)
 
-        if (childAdapterPosition == 0) {
-            outRect.left = marginStartEnd
-        } else if (childAdapterPosition == childAdapterCount - 1) {
-            outRect.right = marginStartEnd
+        if(isRTL) {
+            outRect.right = marginBetweenItems
+
+            if (childAdapterPosition == 0) {
+                outRect.right = marginStartEnd
+            } else if (childAdapterPosition == childAdapterCount - 1) {
+                outRect.left = marginStartEnd
+            }
+        } else {
+            outRect.left = marginBetweenItems
+
+            if (childAdapterPosition == 0) {
+                outRect.left = marginStartEnd
+            } else if (childAdapterPosition == childAdapterCount - 1) {
+                outRect.right = marginStartEnd
+            }
         }
     }
 }
