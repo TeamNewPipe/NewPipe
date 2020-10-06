cbf364f24f775122555122d2705332a086b27eba
vkay94 <viktor.krez94@gmail.com>
2020-10-06 21:17:52 +0200

    Enqueue: Renamed string resource

--- app/src/main/java/org/schabi/newpipe/util/NavigationHelper.java
+++ app/src/main/java/org/schabi/newpipe/util/NavigationHelper.java
@@ -196,7 +196,7 @@ public final class NavigationHelper {
                                             final boolean selectOnAppend,
                                             final boolean resumePlayback) {
 
-        Toast.makeText(context, R.string.player_append, Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, R.string.enqueued, Toast.LENGTH_SHORT).show();
         final Intent intent = getPlayerEnqueueIntent(
                 context, MainPlayer.class, queue, selectOnAppend, resumePlayback);
 
@@ -217,7 +217,7 @@ public final class NavigationHelper {
             return;
         }
 
-        Toast.makeText(context, R.string.player_append, Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, R.string.enqueued, Toast.LENGTH_SHORT).show();
         final Intent intent = getPlayerEnqueueIntent(
                 context, MainPlayer.class, queue, selectOnAppend, resumePlayback);
         intent.putExtra(VideoPlayer.PLAYER_TYPE, VideoPlayer.PLAYER_TYPE_POPUP);
@@ -233,7 +233,7 @@ public final class NavigationHelper {
                                                  final PlayQueue queue,
                                                  final boolean selectOnAppend,
                                                  final boolean resumePlayback) {
-        Toast.makeText(context, R.string.player_append, Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, R.string.enqueued, Toast.LENGTH_SHORT).show();
         final Intent intent = getPlayerEnqueueIntent(
                 context, MainPlayer.class, queue, selectOnAppend, resumePlayback);
         intent.putExtra(VideoPlayer.PLAYER_TYPE, VideoPlayer.PLAYER_TYPE_AUDIO);
