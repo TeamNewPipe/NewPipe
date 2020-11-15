37ceddd11b7260055ed525b43ce86111a47dfe3b
vkay94 <viktor.krez94@gmail.com>
2020-11-15 14:08:41 +0100

    Add progressTime field to some stream entries (database)

--- app/src/main/java/org/schabi/newpipe/database/stream/StreamStatisticsEntry.kt
+++ app/src/main/java/org/schabi/newpipe/database/stream/StreamStatisticsEntry.kt
@@ -2,23 +2,27 @@ package org.schabi.newpipe.database.stream
 
 import androidx.room.ColumnInfo
 import androidx.room.Embedded
-import java.time.OffsetDateTime
 import org.schabi.newpipe.database.LocalItem
 import org.schabi.newpipe.database.history.model.StreamHistoryEntity
 import org.schabi.newpipe.database.stream.model.StreamEntity
+import org.schabi.newpipe.database.stream.model.StreamStateEntity.STREAM_PROGRESS_TIME
 import org.schabi.newpipe.extractor.stream.StreamInfoItem
+import java.time.OffsetDateTime
 
 class StreamStatisticsEntry(
-        @Embedded
+    @Embedded
     val streamEntity: StreamEntity,
 
-        @ColumnInfo(name = StreamHistoryEntity.JOIN_STREAM_ID)
+    @ColumnInfo(name = STREAM_PROGRESS_TIME, defaultValue = "0")
+    val progressTime: Long,
+
+    @ColumnInfo(name = StreamHistoryEntity.JOIN_STREAM_ID)
     val streamId: Long,
 
-        @ColumnInfo(name = STREAM_LATEST_DATE)
+    @ColumnInfo(name = STREAM_LATEST_DATE)
     val latestAccessDate: OffsetDateTime,
 
-        @ColumnInfo(name = STREAM_WATCH_COUNT)
+    @ColumnInfo(name = STREAM_WATCH_COUNT)
     val watchCount: Long
 ) : LocalItem {
     fun toStreamInfoItem(): StreamInfoItem {
