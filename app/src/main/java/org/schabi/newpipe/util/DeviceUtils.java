835ff804326deddb52dea2281769901764b49a0a
GarbageHamburger <63806108+GarbageHamburger@users.noreply.github.com>
2020-11-25 04:47:50 +0100

    Add "Screen layout" dropdown
    Some ROMs allow users to set their screen density. Beyond a certain density,
    NewPipe treats screens with high density as "tablet". This can be undesirable.
    This patch adds a dropdown to the Appearance menu to allow users to force
    the screen layout to specific values.

--- app/src/main/java/org/schabi/newpipe/util/DeviceUtils.java
+++ app/src/main/java/org/schabi/newpipe/util/DeviceUtils.java
@@ -10,8 +10,10 @@ import android.view.KeyEvent;
 
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
+import androidx.preference.PreferenceManager;
 
 import org.schabi.newpipe.App;
+import org.schabi.newpipe.R;
 
 public final class DeviceUtils {
 
@@ -21,9 +23,37 @@ public final class DeviceUtils {
     private DeviceUtils() {
     }
 
+    /**
+     * Checks if the screen layout was forced to the given layout.
+     * @param context Application context
+     * @param resId The resource ID of the wanted screen layout
+     * @return true if the layout was forced, false if the layout was forced to something else,
+     * and null if the screen layout is auto
+     */
+    private static Boolean layoutForcedTo(final Context context, final int resId) {
+        final String screenLayoutKey = context.getString(R.string.screen_layout_key);
+        final String autoKey = context.getString(R.string.screen_layout_auto_key);
+        final String wantedKey = context.getString(resId);
+        final String screenLayout = PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(screenLayoutKey, autoKey);
+
+        if (screenLayout.equals(wantedKey)) {
+            return true;
+        } else if (!screenLayout.equals(autoKey)) {
+            return false;
+        }
+
+        return null;
+    }
+
     public static boolean isTv(final Context context) {
         if (isTV != null) {
             return isTV;
+        }
+
+        final Boolean forcedToTv = layoutForcedTo(context, R.string.screen_layout_tv_key);
+        if (forcedToTv != null) {
+            return forcedToTv;
         }
 
         final PackageManager pm = App.getApp().getPackageManager();
@@ -53,6 +83,11 @@ public final class DeviceUtils {
     }
 
     public static boolean isTablet(@NonNull final Context context) {
+        final Boolean forcedToTablet = layoutForcedTo(context, R.string.screen_layout_tablet_key);
+        if (forcedToTablet != null) {
+            return forcedToTablet;
+        }
+
         return (context
                 .getResources()
                 .getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK)
