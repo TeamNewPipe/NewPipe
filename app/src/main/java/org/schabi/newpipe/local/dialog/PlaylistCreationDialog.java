2b417f61f6d0437f037f3efef420ac97eef159e5
Isira Seneviratne <isirasen96@gmail.com>
2021-01-18 07:24:03 +0530

    Use view binding in PlaylistCreationDialog.

--- app/src/main/java/org/schabi/newpipe/local/dialog/PlaylistCreationDialog.java
+++ app/src/main/java/org/schabi/newpipe/local/dialog/PlaylistCreationDialog.java
@@ -2,24 +2,30 @@ package org.schabi.newpipe.local.dialog;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
+import android.view.LayoutInflater;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import org.jetbrains.annotations.NotNull;
 import org.schabi.newpipe.NewPipeDatabase;
 import org.schabi.newpipe.R;
 import org.schabi.newpipe.database.stream.model.StreamEntity;
+import org.schabi.newpipe.databinding.DialogPlaylistNameBinding;
 import org.schabi.newpipe.local.playlist.LocalPlaylistManager;
 
 import java.util.List;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.Disposable;
 
 public final class PlaylistCreationDialog extends PlaylistDialog {
+    private DialogPlaylistNameBinding binding;
+    private Disposable disposable;
+
     public static PlaylistCreationDialog newInstance(final List<StreamEntity> streams) {
         final PlaylistCreationDialog dialog = new PlaylistCreationDialog();
         dialog.setInfo(streams);
@@ -43,27 +49,35 @@ public final class PlaylistCreationDialog extends PlaylistDialog {
             return super.onCreateDialog(savedInstanceState);
         }
 
-        final View dialogView = View.inflate(getContext(), R.layout.dialog_playlist_name, null);
-        final EditText nameInput = dialogView.findViewById(R.id.playlist_name);
+        binding = DialogPlaylistNameBinding.inflate(LayoutInflater.from(requireContext()));
 
-        final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getContext())
+        final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(requireContext())
                 .setTitle(R.string.create_playlist)
-                .setView(dialogView)
+                .setView(binding.getRoot())
                 .setCancelable(true)
                 .setNegativeButton(R.string.cancel, null)
                 .setPositiveButton(R.string.create, (dialogInterface, i) -> {
-                    final String name = nameInput.getText().toString();
+                    final String name = binding.playlistName.getText().toString();
                     final LocalPlaylistManager playlistManager =
-                            new LocalPlaylistManager(NewPipeDatabase.getInstance(getContext()));
+                            new LocalPlaylistManager(NewPipeDatabase.getInstance(requireContext()));
                     final Toast successToast = Toast.makeText(getActivity(),
                             R.string.playlist_creation_success,
                             Toast.LENGTH_SHORT);
 
-                    playlistManager.createPlaylist(name, getStreams())
+                    disposable = playlistManager.createPlaylist(name, getStreams())
                             .observeOn(AndroidSchedulers.mainThread())
                             .subscribe(longs -> successToast.show());
                 });
 
         return dialogBuilder.create();
+    }
+
+    @Override
+    public void onDismiss(@NonNull @NotNull DialogInterface dialog) {
+        super.onDismiss(dialog);
+        if (disposable != null) {
+            disposable.dispose();
+        }
+        binding = null;
     }
 }
