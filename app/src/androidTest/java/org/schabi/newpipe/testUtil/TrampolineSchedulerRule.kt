56ea526cce7b2c1d1018236eb2c22d97443ddf72
XiangRongLin <41164160+XiangRongLin@users.noreply.github.com>
2021-02-05 20:13:16 +0100

    Add instrumented tests for LocalPlaylistManager.createPlaylist

---
+++ app/src/androidTest/java/org/schabi/newpipe/testUtil/TrampolineSchedulerRule.kt
@@ -0,0 +1,34 @@
+package org.schabi.newpipe.testUtil
+
+import io.reactivex.rxjava3.android.plugins.RxAndroidPlugins
+import io.reactivex.rxjava3.plugins.RxJavaPlugins
+import io.reactivex.rxjava3.schedulers.Schedulers
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+
+/**
+ * Always run on [Schedulers.trampoline]
+ */
+class TrampolineSchedulerRule : TestRule {
+
+    private val scheduler = Schedulers.trampoline()
+
+    override fun apply(base: Statement, description: Description): Statement =
+            object : Statement() {
+                override fun evaluate() {
+                    try {
+                        RxJavaPlugins.setComputationSchedulerHandler { scheduler }
+                        RxJavaPlugins.setIoSchedulerHandler { scheduler }
+                        RxJavaPlugins.setNewThreadSchedulerHandler { scheduler }
+                        RxJavaPlugins.setSingleSchedulerHandler { scheduler }
+                        RxAndroidPlugins.setInitMainThreadSchedulerHandler { scheduler }
+
+                        base.evaluate()
+                    } finally {
+                        RxJavaPlugins.reset()
+                        RxAndroidPlugins.reset()
+                    }
+                }
+            }
+}
