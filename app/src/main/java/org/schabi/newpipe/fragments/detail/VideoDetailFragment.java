6ae34b818a69a2d22e16face365a9f2d8e256b43
sherlockbeard <b417044@iiit-bh.ac.in>
2021-04-04 18:26:21 +0530

    change

--- app/src/main/java/org/schabi/newpipe/fragments/detail/VideoDetailFragment.java
+++ app/src/main/java/org/schabi/newpipe/fragments/detail/VideoDetailFragment.java
@@ -1325,7 +1325,6 @@ public final class VideoDetailFragment
         if (binding == null || activity == null) {
             return;
         }
-
         binding.detailThumbnailImageView.setImageDrawable(
                 AppCompatResources.getDrawable(requireContext(), imageResource));
         animate(binding.detailThumbnailImageView, false, 0, AnimationType.ALPHA,
