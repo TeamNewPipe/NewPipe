73d7e431210a8e3977e2d8f8612c94a23ac03012
Douile <25043847+Douile@users.noreply.github.com>
2021-04-07 22:12:02 +0100

    Refactor into a single function

--- app/src/main/java/org/schabi/newpipe/player/Player.java
+++ app/src/main/java/org/schabi/newpipe/player/Player.java
@@ -1593,30 +1593,7 @@ public final class Player implements
         }
 
         if (isQueueVisible) {
-
-            final int currentStream = playQueue.getIndex();
-            int before = 0;
-            int after = 0;
-
-            final List<PlayQueueItem> streams = playQueue.getStreams();
-            final int nStreams = streams.size();
-
-            for (int i = 0; i < nStreams; i++) {
-                if (i < currentStream) {
-                    before += streams.get(i).getDuration();
-                } else {
-                    after += streams.get(i).getDuration();
-                }
-            }
-
-            before *= 1000;
-            after *= 1000;
-
-            binding.itemsListHeaderDuration.setText(
-                    String.format("%s/%s",
-                            getTimeString(currentProgress + before),
-                            getTimeString(before + after)
-                    ));
+            updateQueueTime(currentProgress);
         }
 
         final boolean showThumbnail = prefs.getBoolean(
@@ -3003,29 +2980,7 @@ public final class Player implements
 
         binding.itemsList.scrollToPosition(playQueue.getIndex());
 
-        final int currentStream = playQueue.getIndex();
-        int before = 0;
-        int after = 0;
-
-        final List<PlayQueueItem> streams = playQueue.getStreams();
-        final int nStreams = streams.size();
-
-        for (int i = 0; i < nStreams; i++) {
-            if (i < currentStream) {
-                before += streams.get(i).getDuration();
-            } else {
-                after += streams.get(i).getDuration();
-            }
-        }
-
-        before *= 1000;
-        after *= 1000;
-
-        binding.itemsListHeaderDuration.setText(
-                String.format("%s/%s",
-                        getTimeString((int) simpleExoPlayer.getCurrentPosition() + before),
-                        getTimeString(before + after)
-                ));
+        updateQueueTime((int) simpleExoPlayer.getCurrentPosition());
     }
 
     private void buildQueue() {
@@ -3247,6 +3202,32 @@ public final class Player implements
 
         buildPlaybackSpeedMenu();
         binding.playbackSpeed.setVisibility(View.VISIBLE);
+    }
+
+    private void updateQueueTime(final int currentTime) {
+        final int currentStream = playQueue.getIndex();
+        int before = 0;
+        int after = 0;
+
+        final List<PlayQueueItem> streams = playQueue.getStreams();
+        final int nStreams = streams.size();
+
+        for (int i = 0; i < nStreams; i++) {
+            if (i < currentStream) {
+                before += streams.get(i).getDuration();
+            } else {
+                after += streams.get(i).getDuration();
+            }
+        }
+
+        before *= 1000;
+        after *= 1000;
+
+        binding.itemsListHeaderDuration.setText(
+                String.format("%s/%s",
+                        getTimeString(currentTime + before),
+                        getTimeString(before + after)
+                ));
     }
     //endregion
 
