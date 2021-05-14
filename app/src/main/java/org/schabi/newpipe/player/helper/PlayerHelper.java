95f61542b5affadfc9ddb648244c653c099ce847
Robin <robin.gawenda@ondamedia.de>
2021-05-14 14:47:53 +0200

    TEST: Downgrade ExoPlayer to 2.12

--- app/src/main/java/org/schabi/newpipe/player/helper/PlayerHelper.java
+++ app/src/main/java/org/schabi/newpipe/player/helper/PlayerHelper.java
@@ -23,7 +23,7 @@ import com.google.android.exoplayer2.Player.RepeatMode;
 import com.google.android.exoplayer2.SeekParameters;
 import com.google.android.exoplayer2.text.CaptionStyleCompat;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
-import com.google.android.exoplayer2.trackselection.ExoTrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
 import com.google.android.exoplayer2.ui.AspectRatioFrameLayout;
 import com.google.android.exoplayer2.ui.AspectRatioFrameLayout.ResizeMode;
 import com.google.android.exoplayer2.util.MimeTypes;
@@ -323,7 +323,7 @@ public final class PlayerHelper {
         return 60000;
     }
 
-    public static ExoTrackSelection.Factory getQualitySelector() {
+    public static TrackSelection.Factory getQualitySelector() {
         return new AdaptiveTrackSelection.Factory(
                 1000,
                 AdaptiveTrackSelection.DEFAULT_MAX_DURATION_FOR_QUALITY_DECREASE_MS,
