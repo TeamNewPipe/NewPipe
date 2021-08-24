1b05c404d581c12c1cc217223ee0596b828475b5
ktprograms <ktprograms@gmail.com>
2021-08-24 19:15:53 +0800

    Remove Details option in Main Player Queue menu

--- app/src/main/java/org/schabi/newpipe/player/Player.java
+++ app/src/main/java/org/schabi/newpipe/player/Player.java
@@ -3043,17 +3043,14 @@ public final class Player implements
         final PopupMenu popupMenu = new PopupMenu(themeWrapper, view);
         popupMenu.inflate(R.menu.menu_play_queue_item);
 
+        // Don't show Details in Main Player Popup
+        popupMenu.getMenu().findItem(R.id.menu_item_details).setVisible(false);
+
         popupMenu.setOnMenuItemClickListener(menuItem -> {
             switch (menuItem.getItemId()) {
                 case R.id.menu_item_remove:
                     final int index = playQueue.indexOf(item);
                     playQueue.remove(index);
-                    return true;
-                case R.id.menu_item_details:
-                    // playQueue is null since we don't want any queue change
-                    NavigationHelper.openVideoDetail(context, item.getServiceId(),
-                            item.getUrl(), item.getTitle(), null,
-                            false);
                     return true;
                 case R.id.menu_item_append_playlist:
                     final PlaylistAppendDialog d = PlaylistAppendDialog.fromPlayQueueItems(
