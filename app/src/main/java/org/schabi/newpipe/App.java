669a35bc7875cee62229c1c75b2543e7b045147a
evermind <evermind@tuxfamily.org>
2021-08-29 08:23:56 +0200

    check immediately for updates if user enables 'check for updates setting'
    - Convert CheckForNewAppVersion to IntentService
    - reset expire date to 0 after user enables check for updates setting

--- app/src/main/java/org/schabi/newpipe/App.java
+++ app/src/main/java/org/schabi/newpipe/App.java
@@ -5,7 +5,6 @@ import android.content.SharedPreferences;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.core.app.NotificationChannelCompat;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.multidex.MultiDexApplication;
@@ -35,13 +34,14 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.exceptions.CompositeException;
 import io.reactivex.rxjava3.exceptions.MissingBackpressureException;
 import io.reactivex.rxjava3.exceptions.OnErrorNotImplementedException;
 import io.reactivex.rxjava3.exceptions.UndeliverableException;
 import io.reactivex.rxjava3.functions.Consumer;
 import io.reactivex.rxjava3.plugins.RxJavaPlugins;
+
+import static org.schabi.newpipe.CheckForNewAppVersion.startNewVersionCheckService;
 
 /*
  * Copyright (C) Hans-Christoph Steiner 2016 <hans@eds.org>
@@ -65,9 +65,6 @@ public class App extends MultiDexApplication {
     public static final String PACKAGE_NAME = BuildConfig.APPLICATION_ID;
     private static final String TAG = App.class.toString();
     private static App app;
-
-    @Nullable
-    private Disposable disposable = null;
 
     @NonNull
     public static App getApp() {
@@ -110,14 +107,11 @@ public class App extends MultiDexApplication {
         configureRxJavaErrorHandler();
 
         // Check for new version
-        disposable = CheckForNewAppVersion.checkNewVersion(this);
+        startNewVersionCheckService();
     }
 
     @Override
     public void onTerminate() {
-        if (disposable != null) {
-            disposable.dispose();
-        }
         super.onTerminate();
         PicassoHelper.terminate();
     }
