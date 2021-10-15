07bf1ecaca30a518e055efff4e32c279ec2d863b
golfinq <golfinqz@gmail.com>
2021-10-15 00:29:22 -0400

    Fixed behavior when scrolling

--- app/src/main/java/org/schabi/newpipe/info_list/holder/CommentsInfoItemHolder.java
+++ app/src/main/java/org/schabi/newpipe/info_list/holder/CommentsInfoItemHolder.java
@@ -70,7 +70,9 @@ public class CommentsInfoItemHolder extends CommentsMiniInfoItemHolder {
 
         repliesView.setMinimumHeight(100);
         repliesView.setHasFixedSize(true);
+        comment.setRepliesOpen(true);
         buttonView.setVisibility(View.GONE);
+        repliesView.setVisibility(View.VISIBLE);
     }
 
     @Override
@@ -88,8 +90,12 @@ public class CommentsInfoItemHolder extends CommentsMiniInfoItemHolder {
         itemHeartView.setVisibility(item.isHeartedByUploader() ? View.VISIBLE : View.GONE);
         itemPinnedView.setVisibility(item.isPinned() ? View.VISIBLE : View.GONE);
         if (item.getReplies() == null) {
+            repliesView.setVisibility(View.GONE);
             showReplies.setVisibility(View.GONE);
+        } else if (item.getRepliesOpen()) {
+            addReplies(showReplies, item);
         } else {
+            repliesView.setVisibility(View.GONE);
             showReplies.setVisibility(View.VISIBLE);
             showReplies.setOnClickListener(v -> addReplies(v, item));
         }
