c6d000362c9ff877aab7d131bd09512b5d6cc6d6
XiangRongLin <41164160+XiangRongLin@users.noreply.github.com>
2022-01-14 11:34:21 +0100

    Replace JUnit asserts with AssertJ in HistoryRecordManagerTest
    They provide a wider range of assertions, which allow for more detailed error messages.
    Also convert SearchHistoryEntry to kotlin data class for better error messages, since toString() is implemented.

--- app/src/main/java/org/schabi/newpipe/database/history/model/SearchHistoryEntry.kt
+++ app/src/main/java/org/schabi/newpipe/database/history/model/SearchHistoryEntry.kt
@@ -1,79 +1,40 @@
-package org.schabi.newpipe.database.history.model;
+package org.schabi.newpipe.database.history.model
 
-import androidx.room.ColumnInfo;
-import androidx.room.Entity;
-import androidx.room.Ignore;
-import androidx.room.Index;
-import androidx.room.PrimaryKey;
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import java.time.OffsetDateTime
 
-import java.time.OffsetDateTime;
-
-import static org.schabi.newpipe.database.history.model.SearchHistoryEntry.SEARCH;
-
-@Entity(tableName = SearchHistoryEntry.TABLE_NAME,
-        indices = {@Index(value = SEARCH)})
-public class SearchHistoryEntry {
-    public static final String ID = "id";
-    public static final String TABLE_NAME = "search_history";
-    public static final String SERVICE_ID = "service_id";
-    public static final String CREATION_DATE = "creation_date";
-    public static final String SEARCH = "search";
-
+@Entity(
+    tableName = SearchHistoryEntry.TABLE_NAME,
+    indices = [Index(value = [SearchHistoryEntry.SEARCH])]
+)
+data class SearchHistoryEntry(
+    @field:ColumnInfo(name = CREATION_DATE) var creationDate: OffsetDateTime,
+    @field:ColumnInfo(
+        name = SERVICE_ID
+    ) var serviceId: Int,
+    @field:ColumnInfo(name = SEARCH) var search: String
+) {
     @ColumnInfo(name = ID)
     @PrimaryKey(autoGenerate = true)
-    private long id;
-
-    @ColumnInfo(name = CREATION_DATE)
-    private OffsetDateTime creationDate;
-
-    @ColumnInfo(name = SERVICE_ID)
-    private int serviceId;
-
-    @ColumnInfo(name = SEARCH)
-    private String search;
-
-    public SearchHistoryEntry(final OffsetDateTime creationDate, final int serviceId,
-                              final String search) {
-        this.serviceId = serviceId;
-        this.creationDate = creationDate;
-        this.search = search;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(final long id) {
-        this.id = id;
-    }
-
-    public OffsetDateTime getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(final OffsetDateTime creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public int getServiceId() {
-        return serviceId;
-    }
-
-    public void setServiceId(final int serviceId) {
-        this.serviceId = serviceId;
-    }
-
-    public String getSearch() {
-        return search;
-    }
-
-    public void setSearch(final String search) {
-        this.search = search;
-    }
+    var id: Long = 0
 
     @Ignore
-    public boolean hasEqualValues(final SearchHistoryEntry otherEntry) {
-        return getServiceId() == otherEntry.getServiceId()
-                && getSearch().equals(otherEntry.getSearch());
+    fun hasEqualValues(otherEntry: SearchHistoryEntry): Boolean {
+        return (
+            serviceId == otherEntry.serviceId &&
+                search == otherEntry.search
+            )
+    }
+
+    companion object {
+        const val ID = "id"
+        const val TABLE_NAME = "search_history"
+        const val SERVICE_ID = "service_id"
+        const val CREATION_DATE = "creation_date"
+        const val SEARCH = "search"
     }
 }
