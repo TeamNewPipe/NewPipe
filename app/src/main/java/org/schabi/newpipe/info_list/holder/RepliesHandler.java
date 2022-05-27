a59bc0c6573b59445518918489593803964c4f68
DUOLabs333 <dvdugo333@gmail.com>
2022-05-27 18:55:53 -0400

    Try something else

--- app/src/main/java/org/schabi/newpipe/info_list/holder/RepliesHandler.java
+++ app/src/main/java/org/schabi/newpipe/info_list/holder/RepliesHandler.java
@@ -173,6 +173,7 @@ public class RepliesHandler {
              addRepliesToUI(parentInfoItem);
         } else {
             cachedReplies.clear();
+            repliesView.setVisibility(View.GONE);
         }
     }
 
