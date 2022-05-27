cf7aeb4e21625226c79a908ff1eb2374bf7bb22e
DUOLabs333 <dvdugo333@gmail.com>
2022-05-27 16:25:57 -0400

    Fix TextView import in CommentsInfoItemHolder.java

--- app/src/main/java/org/schabi/newpipe/info_list/holder/CommentsInfoItemHolder.java
+++ app/src/main/java/org/schabi/newpipe/info_list/holder/CommentsInfoItemHolder.java
@@ -3,7 +3,7 @@ package org.schabi.newpipe.info_list.holder;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import org.schabi.newpipe.views.TextView;
+import android.widget.TextView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import org.schabi.newpipe.R;
