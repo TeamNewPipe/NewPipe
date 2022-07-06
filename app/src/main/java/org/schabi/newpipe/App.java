7cb137ae8d4e3372d453048170d4a6bf6b9771df
litetex <40789489+litetex@users.noreply.github.com>
2022-07-06 16:30:56 -0400

    Remove MultiDex

--- app/src/main/java/org/schabi/newpipe/App.java
+++ app/src/main/java/org/schabi/newpipe/App.java
@@ -1,5 +1,6 @@
 package org.schabi.newpipe;
 
+import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
@@ -7,7 +8,6 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationChannelCompat;
 import androidx.core.app.NotificationManagerCompat;
-import androidx.multidex.MultiDexApplication;
 import androidx.preference.PreferenceManager;
 
 import com.jakewharton.processphoenix.ProcessPhoenix;
@@ -56,7 +56,7 @@ import io.reactivex.rxjava3.plugins.RxJavaPlugins;
  * along with NewPipe.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-public class App extends MultiDexApplication {
+public class App extends Application {
     public static final String PACKAGE_NAME = BuildConfig.APPLICATION_ID;
     private static final String TAG = App.class.toString();
     private static App app;
