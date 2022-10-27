ea875c59af7c7b9d629b9d5d9c2ae8605c1f5752
Stypox <stypox@pm.me>
2022-10-27 14:10:04 +0200

    Deduplicate isGridLayout calls

--- app/src/main/java/org/schabi/newpipe/local/subscription/SubscriptionViewModel.kt
+++ app/src/main/java/org/schabi/newpipe/local/subscription/SubscriptionViewModel.kt
@@ -1,21 +1,19 @@
 package org.schabi.newpipe.local.subscription
 
 import android.app.Application
-import android.content.res.Configuration
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.preference.PreferenceManager
 import com.xwray.groupie.Group
 import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.processors.BehaviorProcessor
 import io.reactivex.rxjava3.schedulers.Schedulers
-import org.schabi.newpipe.R
 import org.schabi.newpipe.local.feed.FeedDatabaseManager
 import org.schabi.newpipe.local.subscription.item.ChannelItem
 import org.schabi.newpipe.local.subscription.item.FeedGroupCardGridItem
 import org.schabi.newpipe.local.subscription.item.FeedGroupCardItem
 import org.schabi.newpipe.util.DEFAULT_THROTTLE_TIMEOUT
+import org.schabi.newpipe.util.ThemeHelper
 import java.util.concurrent.TimeUnit
 
 class SubscriptionViewModel(application: Application) : AndroidViewModel(application) {
@@ -23,7 +21,9 @@ class SubscriptionViewModel(application: Application) : AndroidViewModel(applica
     private var subscriptionManager = SubscriptionManager(application)
 
     // true -> list view, false -> grid view
-    private val listViewMode = BehaviorProcessor.createDefault(!isGridLayout(application))
+    private val listViewMode = BehaviorProcessor.createDefault(
+        !ThemeHelper.shouldUseGridLayout(application)
+    )
     private val listViewModeFlowable = listViewMode.distinctUntilChanged()
 
     private val mutableStateLiveData = MutableLiveData<SubscriptionState>()
@@ -73,25 +73,5 @@ class SubscriptionViewModel(application: Application) : AndroidViewModel(applica
     sealed class SubscriptionState {
         data class LoadedState(val subscriptions: List<Group>) : SubscriptionState()
         data class ErrorState(val error: Throwable? = null) : SubscriptionState()
-    }
-
-    companion object {
-        private fun isGridLayout(application: Application): Boolean {
-            val listMode = PreferenceManager.getDefaultSharedPreferences(application)
-                .getString(
-                    application.getString(R.string.list_view_mode_key),
-                    application.getString(R.string.list_view_mode_value)
-                )
-
-            return if ("auto" == listMode) {
-                val configuration: Configuration = application.resources.configuration
-                (
-                    configuration.orientation == Configuration.ORIENTATION_LANDSCAPE &&
-                        configuration.isLayoutSizeAtLeast(Configuration.SCREENLAYOUT_SIZE_LARGE)
-                    )
-            } else {
-                "grid" == listMode
-            }
-        }
     }
 }
