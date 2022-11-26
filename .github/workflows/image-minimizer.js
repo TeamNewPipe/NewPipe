225f69b75b2c45729b0346a9d1cb3b6e8e8c9265
yashpalgoyal1304 <yashpalgoyal1304@gmail.com>
2022-11-27 01:56:53 +0530

    Fix value of width

--- .github/workflows/image-minimizer.js
+++ .github/workflows/image-minimizer.js
@@ -86,7 +86,7 @@ module.exports = async ({github, context}) => {
         if (shouldModify) {
             wasMatchModified = true;
             console.log(`Modifying match '${match}'`);
-            return `<img alt="${g1}" src="${g2}" width=${IMG_MAX_WIDTH_PX} />`;
+            return `<img alt="${g1}" src="${g2}" width=${((IMG_MAX_HEIGHT_PX * probeResult.width) / probeResult.height).toFixed(0)} />`;
         }
         
         console.log(`Match '${match}' is ok/will not be modified`);
