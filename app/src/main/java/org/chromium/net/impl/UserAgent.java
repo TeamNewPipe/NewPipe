a5a4a94bf98844508c23db8ee3ed95015d4c2509
Stypox <stypox@pm.me>
2023-01-12 13:23:34 +0100

    Remove any reference to user data in cronet's UserAgent

--- app/src/main/java/org/chromium/net/impl/UserAgent.java
+++ app/src/main/java/org/chromium/net/impl/UserAgent.java
@@ -7,12 +7,8 @@
 package org.chromium.net.impl;
 
 import android.content.Context;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.os.Build;
 
-import java.util.Locale;
+import org.schabi.newpipe.DownloaderImpl;
 
 /**
  * Constructs a User-Agent string.
@@ -24,84 +20,25 @@ import java.util.Locale;
  * </p>
  */
 public final class UserAgent {
-    private static final Object sLock = new Object();
-
-    private static final int VERSION_CODE_UNINITIALIZED = 0;
-    private static int sVersionCode = VERSION_CODE_UNINITIALIZED;
 
     private UserAgent() {}
 
     /**
-     * Constructs a User-Agent string including application name and version,
-     * system build version, model and Id, and Cronet version.
-     * @param context the context to fetch the application name and version
-     *         from.
+     * This method used to return a user agent leaking user information, and so has been replaced.
+     * @param ignoredContext not used but kept for compatibility
      * @return User-Agent string.
      */
-    public static String from(Context context) {
-        StringBuilder builder = new StringBuilder();
-
-        // Our package name and version.
-        builder.append(context.getPackageName());
-        builder.append('/');
-        builder.append(versionFromContext(context));
-
-        // The platform version.
-        builder.append(" (Linux; U; Android ");
-        builder.append(Build.VERSION.RELEASE);
-        builder.append("; ");
-        builder.append(Locale.getDefault().toString());
-
-        String model = Build.MODEL;
-        if (model.length() > 0) {
-            builder.append("; ");
-            builder.append(model);
-        }
-
-        String id = Build.ID;
-        if (id.length() > 0) {
-            builder.append("; Build/");
-            builder.append(id);
-        }
-
-        builder.append(";");
-        appendCronetVersion(builder);
-
-        builder.append(')');
-
-        return builder.toString();
+    public static String from(Context ignoredContext) {
+        return DownloaderImpl.USER_AGENT;
     }
 
     /**
-     * Constructs default QUIC User Agent Id string including Cronet version.
-     * @param context the context to fetch the application name from, unused with the changes of
-     *                the method but kept for signature matches purpose with the method callers
-     *                from other Cronet modules
+     * This method used to return a user agent leaking user information, and so has been replaced.
+     * @param ignoredContext not used but kept for compatibility
      * @return User-Agent string.
      */
     @SuppressWarnings("unused")
-    static String getQuicUserAgentIdFrom(Context context) {
+    static String getQuicUserAgentIdFrom(Context ignoredContext) {
         return "Cronet/" + ImplVersion.getCronetVersion();
-    }
-
-    private static int versionFromContext(Context context) {
-        synchronized (sLock) {
-            if (sVersionCode == VERSION_CODE_UNINITIALIZED) {
-                PackageManager packageManager = context.getPackageManager();
-                String packageName = context.getPackageName();
-                try {
-                    PackageInfo packageInfo = packageManager.getPackageInfo(packageName, 0);
-                    sVersionCode = packageInfo.versionCode;
-                } catch (NameNotFoundException e) {
-                    throw new IllegalStateException("Cannot determine package version");
-                }
-            }
-            return sVersionCode;
-        }
-    }
-
-    private static void appendCronetVersion(StringBuilder builder) {
-        builder.append(" Cronet/");
-        builder.append(ImplVersion.getCronetVersion());
     }
 }
