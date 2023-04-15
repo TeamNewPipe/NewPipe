753a92055c18cbee58e6b7cf4edbdd80b63d56ef
ThetaDev <t.testboy@gmail.com>
2023-04-16 00:58:30 +0200

    feat: add playlist controls to channel tab

--- app/src/main/java/org/schabi/newpipe/fragments/list/channel/ChannelTabFragment.java
+++ app/src/main/java/org/schabi/newpipe/fragments/list/channel/ChannelTabFragment.java
@@ -9,13 +9,24 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import org.schabi.newpipe.R;
+import org.schabi.newpipe.databinding.PlaylistControlBinding;
 import org.schabi.newpipe.error.UserAction;
 import org.schabi.newpipe.extractor.InfoItem;
 import org.schabi.newpipe.extractor.ListExtractor;
 import org.schabi.newpipe.extractor.channel.ChannelTabInfo;
 import org.schabi.newpipe.extractor.linkhandler.ListLinkHandler;
+import org.schabi.newpipe.extractor.stream.StreamInfoItem;
 import org.schabi.newpipe.fragments.list.BaseListInfoFragment;
+import org.schabi.newpipe.player.PlayerType;
+import org.schabi.newpipe.player.playqueue.ChannelTabPlayQueue;
+import org.schabi.newpipe.player.playqueue.PlayQueue;
+import org.schabi.newpipe.util.ChannelTabHelper;
 import org.schabi.newpipe.util.ExtractorHelper;
+import org.schabi.newpipe.util.NavigationHelper;
+
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 import icepick.State;
 import io.reactivex.rxjava3.core.Single;
@@ -26,6 +37,8 @@ public class ChannelTabFragment extends BaseListInfoFragment<InfoItem, ChannelTa
     protected ListLinkHandler tabHandler;
     @State
     protected String channelName;
+
+    private PlaylistControlBinding playlistControlBinding;
 
     public static ChannelTabFragment getInstance(final int serviceId,
                                                  final ListLinkHandler tabHandler,
@@ -59,6 +72,22 @@ public class ChannelTabFragment extends BaseListInfoFragment<InfoItem, ChannelTa
     }
 
     @Override
+    public void onDestroy() {
+        super.onDestroy();
+        playlistControlBinding = null;
+    }
+
+    @Override
+    protected Supplier<View> getListHeaderSupplier() {
+        if (ChannelTabHelper.isStreamsTab(tabHandler)) {
+            playlistControlBinding = PlaylistControlBinding
+                    .inflate(activity.getLayoutInflater(), itemsList, false);
+            return playlistControlBinding::getRoot;
+        }
+        return null;
+    }
+
+    @Override
     protected Single<ChannelTabInfo> loadResult(final boolean forceLoad) {
         return ExtractorHelper.getChannelTab(serviceId, tabHandler, forceLoad);
     }
@@ -71,5 +100,48 @@ public class ChannelTabFragment extends BaseListInfoFragment<InfoItem, ChannelTa
     @Override
     public void setTitle(final String title) {
         super.setTitle(channelName);
+    }
+
+    @Override
+    public void handleResult(final @NonNull ChannelTabInfo result) {
+        super.handleResult(result);
+
+        if (playlistControlBinding != null) {
+            // PlaylistControls should be visible only if there is some item in
+            // infoListAdapter other than header
+            if (infoListAdapter.getItemCount() > 1) {
+                playlistControlBinding.getRoot().setVisibility(View.VISIBLE);
+            } else {
+                playlistControlBinding.getRoot().setVisibility(View.GONE);
+            }
+
+            playlistControlBinding.playlistCtrlPlayAllButton.setOnClickListener(
+                    view -> NavigationHelper.playOnMainPlayer(activity, getPlayQueue()));
+            playlistControlBinding.playlistCtrlPlayPopupButton.setOnClickListener(
+                    view -> NavigationHelper.playOnPopupPlayer(activity, getPlayQueue(), false));
+            playlistControlBinding.playlistCtrlPlayBgButton.setOnClickListener(
+                    view -> NavigationHelper.playOnBackgroundPlayer(activity, getPlayQueue(),
+                            false));
+
+            playlistControlBinding.playlistCtrlPlayPopupButton.setOnLongClickListener(view -> {
+                NavigationHelper.enqueueOnPlayer(activity, getPlayQueue(), PlayerType.POPUP);
+                return true;
+            });
+
+            playlistControlBinding.playlistCtrlPlayBgButton.setOnLongClickListener(view -> {
+                NavigationHelper.enqueueOnPlayer(activity, getPlayQueue(), PlayerType.AUDIO);
+                return true;
+            });
+        }
+    }
+
+    private PlayQueue getPlayQueue() {
+        final List<StreamInfoItem> streamItems = infoListAdapter.getItemsList().stream()
+                .filter(StreamInfoItem.class::isInstance)
+                .map(StreamInfoItem.class::cast)
+                .collect(Collectors.toList());
+
+        return new ChannelTabPlayQueue(currentInfo.getServiceId(), tabHandler,
+                currentInfo.getNextPage(), streamItems, 0);
     }
 }
