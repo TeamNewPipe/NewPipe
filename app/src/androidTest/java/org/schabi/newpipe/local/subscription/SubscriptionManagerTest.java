0d9910cbbec39cd605aba774f2714e93db33ce5b
AudricV <74829229+AudricV@users.noreply.github.com>
2023-08-23 23:23:26 +0200

    Fix SubscriptionManagerTest tests
    The breakage of these tests is related to the channel tabs changes.
    
    The testRememberRecentStreams test method has been removed, as it doesn't seem
    to be relevant anymore to managing subscriptions.

--- app/src/androidTest/java/org/schabi/newpipe/local/subscription/SubscriptionManagerTest.java
+++ app/src/androidTest/java/org/schabi/newpipe/local/subscription/SubscriptionManagerTest.java
@@ -10,19 +10,13 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.schabi.newpipe.database.AppDatabase;
 import org.schabi.newpipe.database.feed.model.FeedGroupEntity;
-import org.schabi.newpipe.database.stream.model.StreamEntity;
 import org.schabi.newpipe.database.subscription.SubscriptionEntity;
 import org.schabi.newpipe.extractor.channel.ChannelInfo;
 import org.schabi.newpipe.extractor.exceptions.ExtractionException;
-import org.schabi.newpipe.extractor.localization.DateWrapper;
-import org.schabi.newpipe.extractor.stream.StreamInfoItem;
-import org.schabi.newpipe.extractor.stream.StreamType;
 import org.schabi.newpipe.testUtil.TestDatabase;
 import org.schabi.newpipe.testUtil.TrampolineSchedulerRule;
 
 import java.io.IOException;
-import java.time.OffsetDateTime;
-import java.util.Comparator;
 import java.util.List;
 
 public class SubscriptionManagerTest {
@@ -58,7 +52,7 @@ public class SubscriptionManagerTest {
         final ChannelInfo info = ChannelInfo.getInfo("https://www.youtube.com/c/3blue1brown");
         final SubscriptionEntity subscription = SubscriptionEntity.from(info);
 
-        manager.insertSubscription(subscription, info);
+        manager.insertSubscription(subscription);
         final SubscriptionEntity readSubscription = getAssertOneSubscriptionEntity();
 
         // the uid has changed, since the uid is chosen upon inserting, but the rest should match
@@ -76,7 +70,7 @@ public class SubscriptionManagerTest {
         final SubscriptionEntity subscription = SubscriptionEntity.from(info);
         subscription.setNotificationMode(0);
 
-        manager.insertSubscription(subscription, info);
+        manager.insertSubscription(subscription);
         manager.updateNotificationMode(subscription.getServiceId(), subscription.getUrl(), 1)
                 .blockingAwait();
         final SubscriptionEntity anotherSubscription = getAssertOneSubscriptionEntity();
@@ -84,36 +78,5 @@ public class SubscriptionManagerTest {
         assertEquals(0, subscription.getNotificationMode());
         assertEquals(subscription.getUrl(), anotherSubscription.getUrl());
         assertEquals(1, anotherSubscription.getNotificationMode());
-    }
-
-    @Test
-    public void testRememberRecentStreams() throws ExtractionException, IOException {
-        final ChannelInfo info = ChannelInfo.getInfo("https://www.youtube.com/c/Polyphia");
-        final List<StreamInfoItem> relatedItems = List.of(
-                new StreamInfoItem(0, "a", "b", StreamType.VIDEO_STREAM),
-                new StreamInfoItem(1, "c", "d", StreamType.AUDIO_STREAM),
-                new StreamInfoItem(2, "e", "f", StreamType.AUDIO_LIVE_STREAM),
-                new StreamInfoItem(3, "g", "h", StreamType.LIVE_STREAM));
-        relatedItems.forEach(item -> {
-            // these two fields must be non-null for the insert to succeed
-            item.setUploaderUrl(info.getUrl());
-            item.setUploaderName(info.getName());
-            // the upload date must not be too much in the past for the item to actually be inserted
-            item.setUploadDate(new DateWrapper(OffsetDateTime.now()));
-        });
-        info.setRelatedItems(relatedItems);
-        final SubscriptionEntity subscription = SubscriptionEntity.from(info);
-
-        manager.insertSubscription(subscription, info);
-        final List<StreamEntity> streams = database.streamDAO().getAll().blockingFirst();
-
-        assertEquals(4, streams.size());
-        streams.sort(Comparator.comparing(StreamEntity::getServiceId));
-        for (int i = 0; i < 4; i++) {
-            assertEquals(relatedItems.get(0).getServiceId(), streams.get(0).getServiceId());
-            assertEquals(relatedItems.get(0).getUrl(), streams.get(0).getUrl());
-            assertEquals(relatedItems.get(0).getName(), streams.get(0).getTitle());
-            assertEquals(relatedItems.get(0).getStreamType(), streams.get(0).getStreamType());
-        }
     }
 }
