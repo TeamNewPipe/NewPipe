a1f2b7f8e83e0427a099d7c12b3fc416d96616f7
Isira Seneviratne <isirasen96@gmail.com>
2023-09-18 05:14:15 +0530

    Switch to Files.createDirectories()

--- app/src/main/java/org/schabi/newpipe/streams/io/StoredDirectoryHelper.java
+++ app/src/main/java/org/schabi/newpipe/streams/io/StoredDirectoryHelper.java
@@ -180,9 +180,12 @@ public class StoredDirectoryHelper {
      */
     public boolean mkdirs() {
         if (docTree == null) {
-            // TODO: Use Files.createDirectories() when AGP 8.1 is available:
-            // https://issuetracker.google.com/issues/282544786
-            return Files.exists(ioTree) || ioTree.toFile().mkdirs();
+            try {
+                Files.createDirectories(ioTree);
+            } catch (final IOException e) {
+                Log.e(TAG, "Error while creating directories at " + ioTree, e);
+            }
+            return Files.exists(ioTree);
         }
 
         if (docTree.exists()) {
