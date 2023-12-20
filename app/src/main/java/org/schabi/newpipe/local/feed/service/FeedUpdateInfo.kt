80f33daeeb246ee548dc707d3798ae382d137079
Stypox <stypox@pm.me>
2023-12-20 20:22:45 +0100

    Fix OutOfMemory when fetching feed
    Reduced memory footprint of FeedUpdateInfo objects. Those objects might stay around for a while and accumulate (up to BUFFER_COUNT_BEFORE_INSERT = 20 at the moment), so in order not to fill up the memory it's better to keep as little data as possible.
    Previously ChannelInfo data was stored, causing ReadyChannelTabLinkHandler objects to be also stored uselessly (and those channel tabs contain prefetched JSON data which used ~700KB of memory).

--- app/src/main/java/org/schabi/newpipe/local/feed/service/FeedUpdateInfo.kt
+++ app/src/main/java/org/schabi/newpipe/local/feed/service/FeedUpdateInfo.kt
@@ -3,29 +3,48 @@ package org.schabi.newpipe.local.feed.service
 import org.schabi.newpipe.database.subscription.NotificationMode
 import org.schabi.newpipe.database.subscription.SubscriptionEntity
 import org.schabi.newpipe.extractor.Info
+import org.schabi.newpipe.extractor.channel.ChannelInfo
 import org.schabi.newpipe.extractor.stream.StreamInfoItem
+import org.schabi.newpipe.util.image.ImageStrategy
 
+/**
+ * Instances of this class might stay around in memory for some time while fetching the feed,
+ * because of [FeedLoadManager.BUFFER_COUNT_BEFORE_INSERT]. Therefore this class should contain
+ * as little data as possible to avoid out of memory errors. In particular, avoid storing whole
+ * [ChannelInfo] objects, as they might contain raw JSON info in ready channel tabs link handlers.
+ */
 data class FeedUpdateInfo(
     val uid: Long,
     @NotificationMode
     val notificationMode: Int,
     val name: String,
     val avatarUrl: String,
-    val originalInfo: Info,
+    val url: String,
+    val serviceId: Int,
+    // description and subscriberCount are null if the constructor info is from the fast feed method
+    val description: String?,
+    val subscriberCount: Long?,
     val streams: List<StreamInfoItem>,
     val errors: List<Throwable>,
 ) {
     constructor(
         subscription: SubscriptionEntity,
-        originalInfo: Info,
+        info: Info,
         streams: List<StreamInfoItem>,
         errors: List<Throwable>,
     ) : this(
         uid = subscription.uid,
         notificationMode = subscription.notificationMode,
-        name = subscription.name,
-        avatarUrl = subscription.avatarUrl,
-        originalInfo = originalInfo,
+        name = info.name,
+        avatarUrl = (info as? ChannelInfo)?.avatars?.let {
+            // if the newly fetched info is not from fast feed, then it contains updated avatars
+            ImageStrategy.imageListToDbUrl(it)
+        } ?: subscription.avatarUrl,
+        url = info.url,
+        serviceId = info.serviceId,
+        // there is no description and subscriberCount in the fast feed
+        description = (info as? ChannelInfo)?.description,
+        subscriberCount = (info as? ChannelInfo)?.subscriberCount,
         streams = streams,
         errors = errors,
     )
@@ -34,7 +53,7 @@ data class FeedUpdateInfo(
      * Integer id, can be used as notification id, etc.
      */
     val pseudoId: Int
-        get() = originalInfo.url.hashCode()
+        get() = url.hashCode()
 
     lateinit var newStreams: List<StreamInfoItem>
 }
