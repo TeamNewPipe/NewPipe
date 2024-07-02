ceb6836d46561fb62962ed1a51954a8133a74433
Isira Seneviratne <isirasen96@gmail.com>
2024-07-02 18:19:17 +0530

    Add comment view model

--- app/src/main/java/org/schabi/newpipe/fragments/list/comments/CommentsFragment.kt
+++ app/src/main/java/org/schabi/newpipe/fragments/list/comments/CommentsFragment.kt
@@ -2,60 +2,37 @@ package org.schabi.newpipe.fragments.list.comments
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import androidx.paging.cachedIn
+import androidx.lifecycle.viewmodel.compose.viewModel
 import org.schabi.newpipe.compose.comment.CommentSection
 import org.schabi.newpipe.compose.theme.AppTheme
-import org.schabi.newpipe.paging.CommentsSource
-import org.schabi.newpipe.util.NO_SERVICE_ID
+import org.schabi.newpipe.util.KEY_SERVICE_ID
+import org.schabi.newpipe.util.KEY_URL
+import org.schabi.newpipe.viewmodels.CommentsViewModel
 
 class CommentsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val arguments = requireArguments()
-        val serviceId = arguments.getInt(SERVICE_ID, NO_SERVICE_ID)
-        val url = arguments.getString(URL)
-
-        return ComposeView(requireContext()).apply {
-            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
-            setContent {
-                val coroutineScope = rememberCoroutineScope()
-                val flow = remember(coroutineScope) {
-                    Pager(PagingConfig(pageSize = 20, enablePlaceholders = false)) {
-                        CommentsSource(serviceId, url, null)
-                    }.flow
-                        .cachedIn(coroutineScope)
-                }
-
-                AppTheme {
-                    CommentSection(commentsData = flow)
-                }
+    ) = ComposeView(requireContext()).apply {
+        setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+        setContent {
+            val viewModel = viewModel<CommentsViewModel>()
+            AppTheme {
+                CommentSection(commentsFlow = viewModel.comments)
             }
         }
     }
 
     companion object {
-        private const val SERVICE_ID = "serviceId"
-        private const val URL = "url"
-
         @JvmStatic
         fun getInstance(serviceId: Int, url: String?) = CommentsFragment().apply {
-            arguments = bundleOf(
-                SERVICE_ID to serviceId,
-                URL to url
-            )
+            arguments = bundleOf(KEY_SERVICE_ID to serviceId, KEY_URL to url)
         }
     }
 }
