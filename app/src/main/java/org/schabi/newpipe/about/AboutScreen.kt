047c288755caf362f24cd292d1241740bb4ce09f
Isira Seneviratne <isirasen96@gmail.com>
2024-07-13 20:01:07 +0530

    Migrate about activity to Jetpack Compose

---
+++ app/src/main/java/org/schabi/newpipe/about/AboutScreen.kt
@@ -0,0 +1,96 @@
+package org.schabi.newpipe.about
+
+import android.content.res.Configuration
+import androidx.collection.intListOf
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.NonRestartableComposable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import my.nanihadesuka.compose.ColumnScrollbar
+import org.schabi.newpipe.R
+import org.schabi.newpipe.compose.theme.AppTheme
+
+private val TITLES = intListOf(R.string.tab_about, R.string.tab_licenses)
+
+@Composable
+@NonRestartableComposable
+fun AboutScreen(padding: PaddingValues) {
+    Column(modifier = Modifier.padding(padding)) {
+        var tabIndex by rememberSaveable { mutableIntStateOf(0) }
+        val pagerState = rememberPagerState { TITLES.size }
+
+        LaunchedEffect(tabIndex) {
+            pagerState.animateScrollToPage(tabIndex)
+        }
+        LaunchedEffect(pagerState.currentPage) {
+            tabIndex = pagerState.currentPage
+        }
+
+        TabRow(selectedTabIndex = tabIndex) {
+            TITLES.forEachIndexed { index, titleId ->
+                Tab(
+                    text = { Text(text = stringResource(titleId)) },
+                    selected = tabIndex == index,
+                    onClick = { tabIndex = index }
+                )
+            }
+        }
+
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) { page ->
+            val scrollState = rememberScrollState()
+
+            ColumnScrollbar(state = scrollState) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp, vertical = 10.dp)
+                        .verticalScroll(scrollState),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    if (page == 0) {
+                        AboutTab()
+                    } else {
+                        LicenseTab()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(name = "Light mode", uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Preview(name = "Dark mode", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun AboutScreenPreview() {
+    AppTheme {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            AboutScreen(PaddingValues(8.dp))
+        }
+    }
+}
