4d9590b6e59af66d77fab86971194fb29ab82e70
Isira Seneviratne <isirasen96@gmail.com>
2024-07-15 04:56:49 +0530

    Fix tab text color

--- app/src/main/java/org/schabi/newpipe/about/AboutScreen.kt
+++ app/src/main/java/org/schabi/newpipe/about/AboutScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -50,7 +51,7 @@ fun AboutScreen(padding: PaddingValues) {
         TabRow(selectedTabIndex = tabIndex) {
             TITLES.forEachIndexed { index, titleId ->
                 Tab(
-                    text = { Text(text = stringResource(titleId)) },
+                    text = { Text(text = stringResource(titleId), color = Color.White) },
                     selected = tabIndex == index,
                     onClick = { tabIndex = index }
                 )
