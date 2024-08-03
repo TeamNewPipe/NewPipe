0cdc31e97efbcc97bae471c39efb78d4b3977e25
Isira Seneviratne <isirasen96@gmail.com>
2024-08-03 05:49:30 +0530

    Implement remaining stream menu items

--- app/src/main/java/org/schabi/newpipe/ui/components/items/stream/StreamMenu.kt
+++ app/src/main/java/org/schabi/newpipe/ui/components/items/stream/StreamMenu.kt
@@ -1,18 +1,28 @@
 package org.schabi.newpipe.ui.components.items.stream
 
+import androidx.annotation.StringRes
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.launch
 import org.schabi.newpipe.R
+import org.schabi.newpipe.database.stream.model.StreamEntity
 import org.schabi.newpipe.download.DownloadDialog
-import org.schabi.newpipe.extractor.stream.StreamInfo
 import org.schabi.newpipe.extractor.stream.StreamInfoItem
+import org.schabi.newpipe.local.dialog.PlaylistAppendDialog
+import org.schabi.newpipe.local.dialog.PlaylistDialog
+import org.schabi.newpipe.player.helper.PlayerHolder
+import org.schabi.newpipe.util.NavigationHelper
 import org.schabi.newpipe.util.SparseItemUtil
 import org.schabi.newpipe.util.external_communication.ShareUtils
+import org.schabi.newpipe.viewmodels.StreamViewModel
 
 @Composable
 fun StreamMenu(
@@ -20,24 +30,60 @@ fun StreamMenu(
     onDismissRequest: () -> Unit
 ) {
     val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+    val streamViewModel = viewModel<StreamViewModel>()
+    val playerHolder = PlayerHolder.getInstance()
 
-    // TODO: Implement remaining click actions
     DropdownMenu(expanded = true, onDismissRequest = onDismissRequest) {
-        DropdownMenuItem(
-            text = { Text(text = stringResource(R.string.start_here_on_background)) },
-            onClick = onDismissRequest
+        if (playerHolder.isPlayQueueReady) {
+            StreamMenuItem(
+                text = R.string.enqueue_stream,
+                onClick = {
+                    onDismissRequest()
+                    SparseItemUtil.fetchItemInfoIfSparse(context, stream) {
+                        NavigationHelper.enqueueOnPlayer(context, it)
+                    }
+                }
+            )
+
+            if (playerHolder.queuePosition < playerHolder.queueSize - 1) {
+                StreamMenuItem(
+                    text = R.string.enqueue_next_stream,
+                    onClick = {
+                        onDismissRequest()
+                        SparseItemUtil.fetchItemInfoIfSparse(context, stream) {
+                            NavigationHelper.enqueueNextOnPlayer(context, it)
+                        }
+                    }
+                )
+            }
+        }
+
+        StreamMenuItem(
+            text = R.string.start_here_on_background,
+            onClick = {
+                onDismissRequest()
+                SparseItemUtil.fetchItemInfoIfSparse(context, stream) {
+                    NavigationHelper.playOnBackgroundPlayer(context, it, true)
+                }
+            }
         )
-        DropdownMenuItem(
-            text = { Text(text = stringResource(R.string.start_here_on_popup)) },
-            onClick = onDismissRequest
+        StreamMenuItem(
+            text = R.string.start_here_on_popup,
+            onClick = {
+                onDismissRequest()
+                SparseItemUtil.fetchItemInfoIfSparse(context, stream) {
+                    NavigationHelper.playOnPopupPlayer(context, it, true)
+                }
+            }
         )
-        DropdownMenuItem(
-            text = { Text(text = stringResource(R.string.download)) },
+        StreamMenuItem(
+            text = R.string.download,
             onClick = {
                 onDismissRequest()
                 SparseItemUtil.fetchStreamInfoAndSaveToDatabase(
                     context, stream.serviceId, stream.url
-                ) { info: StreamInfo ->
+                ) { info ->
                     // TODO: Use an AlertDialog composable instead.
                     val downloadDialog = DownloadDialog(context, info)
                     val fragmentManager = (context as FragmentActivity).supportFragmentManager
@@ -45,31 +91,66 @@ fun StreamMenu(
                 }
             }
         )
-        DropdownMenuItem(
-            text = { Text(text = stringResource(R.string.add_to_playlist)) },
-            onClick = onDismissRequest
+        StreamMenuItem(
+            text = R.string.add_to_playlist,
+            onClick = {
+                onDismissRequest()
+                val list = listOf(StreamEntity(stream))
+                PlaylistDialog.createCorrespondingDialog(context, list) { dialog ->
+                    val tag = if (dialog is PlaylistAppendDialog) "append" else "create"
+                    dialog.show(
+                        (context as FragmentActivity).supportFragmentManager,
+                        "StreamDialogEntry@${tag}_playlist"
+                    )
+                }
+            }
         )
-        DropdownMenuItem(
-            text = { Text(text = stringResource(R.string.share)) },
+        StreamMenuItem(
+            text = R.string.share,
             onClick = {
                 onDismissRequest()
                 ShareUtils.shareText(context, stream.name, stream.url, stream.thumbnails)
             }
         )
-        DropdownMenuItem(
-            text = { Text(text = stringResource(R.string.open_in_browser)) },
+        StreamMenuItem(
+            text = R.string.open_in_browser,
             onClick = {
                 onDismissRequest()
                 ShareUtils.openUrlInBrowser(context, stream.url)
             }
         )
-        DropdownMenuItem(
-            text = { Text(text = stringResource(R.string.mark_as_watched)) },
-            onClick = onDismissRequest
+        StreamMenuItem(
+            text = R.string.mark_as_watched,
+            onClick = {
+                onDismissRequest()
+                coroutineScope.launch {
+                    streamViewModel.markAsWatched(stream)
+                }
+            }
         )
-        DropdownMenuItem(
-            text = { Text(text = stringResource(R.string.show_channel_details)) },
-            onClick = onDismissRequest
+        StreamMenuItem(
+            text = R.string.show_channel_details,
+            onClick = {
+                onDismissRequest()
+                SparseItemUtil.fetchUploaderUrlIfSparse(
+                    context, stream.serviceId, stream.url, stream.uploaderUrl
+                ) { url ->
+                    NavigationHelper.openChannelFragment(context as FragmentActivity, stream, url)
+                }
+            }
         )
     }
+}
+
+@Composable
+private fun StreamMenuItem(
+    @StringRes text: Int,
+    onClick: () -> Unit
+) {
+    DropdownMenuItem(
+        text = {
+            Text(text = stringResource(text), color = MaterialTheme.colorScheme.onBackground)
+        },
+        onClick = onClick
+    )
 }
--- app/src/main/java/org/schabi/newpipe/viewmodels/StreamViewModel.kt
+++ app/src/main/java/org/schabi/newpipe/viewmodels/StreamViewModel.kt
@@ -2,9 +2,11 @@ package org.schabi.newpipe.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import kotlinx.coroutines.rx3.await
 import kotlinx.coroutines.rx3.awaitSingleOrNull
 import org.schabi.newpipe.database.stream.model.StreamStateEntity
 import org.schabi.newpipe.extractor.InfoItem
+import org.schabi.newpipe.extractor.stream.StreamInfoItem
 import org.schabi.newpipe.local.history.HistoryRecordManager
 
 class StreamViewModel(application: Application) : AndroidViewModel(application) {
@@ -12,5 +14,9 @@ class StreamViewModel(application: Application) : AndroidViewModel(application) 
 
     suspend fun getStreamState(infoItem: InfoItem): StreamStateEntity? {
         return historyRecordManager.loadStreamState(infoItem).awaitSingleOrNull()
+    }
+
+    suspend fun markAsWatched(stream: StreamInfoItem) {
+        historyRecordManager.markAsWatched(stream).await()
     }
 }
