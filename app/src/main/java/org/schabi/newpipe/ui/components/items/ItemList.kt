6f5187b34fe8573a699b33ed760e56c13ddefa20
Isira Seneviratne <isirasen96@gmail.com>
2024-08-10 06:25:44 +0530

    Use nested scroll modifier

--- app/src/main/java/org/schabi/newpipe/ui/components/items/ItemList.kt
+++ app/src/main/java/org/schabi/newpipe/ui/components/items/ItemList.kt
@@ -9,7 +9,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
 import androidx.compose.ui.res.stringResource
 import androidx.fragment.app.FragmentActivity
 import androidx.preference.PreferenceManager
@@ -62,21 +65,20 @@ fun ItemList(
     }
 
     val showProgress = DependentPreferenceHelper.getPositionsInListsEnabled(context)
+    val nestedScrollModifier = Modifier.nestedScroll(rememberNestedScrollInteropConnection())
 
     if (mode == ItemViewMode.GRID) {
         // TODO: Implement grid layout using LazyVerticalGrid and LazyVerticalGridScrollbar.
     } else {
-        // Card or list views
-        val listState = rememberLazyListState()
+        val state = rememberLazyListState()
 
-        LazyColumnScrollbar(state = listState) {
-            LazyColumn(state = listState) {
+        LazyColumnScrollbar(state = state) {
+            LazyColumn(modifier = nestedScrollModifier, state = state) {
                 listHeader()
 
                 items(items.size) {
                     val item = items[it]
 
-                    // TODO: Implement card layouts.
                     if (item is StreamInfoItem) {
                         val isSelected = selectedStream == item
                         StreamListItem(
