92e9ea89488622b112db4f6e59577ac782b27faa
Andrei Simon <142477659+andreiDiazSimon@users.noreply.github.com>
2024-08-16 04:53:36 +0800

    Update FragmentStatePagerAdapterMenuWorkaround.java

--- app/src/main/java/androidx/fragment/app/FragmentStatePagerAdapterMenuWorkaround.java
+++ app/src/main/java/androidx/fragment/app/FragmentStatePagerAdapterMenuWorkaround.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+/*this is a comment haha*/
 package androidx.fragment.app;
 
 import android.os.Bundle;
