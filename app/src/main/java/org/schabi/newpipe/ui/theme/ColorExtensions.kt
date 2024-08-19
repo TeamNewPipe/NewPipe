90499b120a8ecec3cb144d6bd5a33b5bd6c10391
toliuweijing <waiging.lau@gmail.com>
2024-08-19 16:11:31 +0800

    boost error hint color

--- app/src/main/java/org/schabi/newpipe/ui/theme/ColorExtensions.kt
+++ app/src/main/java/org/schabi/newpipe/ui/theme/ColorExtensions.kt
@@ -7,15 +7,13 @@ import androidx.compose.ui.graphics.Color
 
 /**
  * Extended color for error hint.
- * See [specification](https://m1.material.io/patterns/errors.html#errors-user-input-errors)
  */
-val md_theme_light_error_hint = Color(0x61000000)
-
-val md_theme_dark_error_hint = Color(0x80FFFFFF)
+val md_theme_light_error_hint = Color(0xCC000000)
+val md_theme_dark_error_hint = Color(0xCCFFFFFF)
 
 val ColorScheme.errorHint: Color
     @Composable get() = if (isSystemInDarkTheme()) {
-        Color(0x80FFFFFF)
+        md_theme_dark_error_hint
     } else {
-        Color(0x61000000)
+        md_theme_light_error_hint
     }
