cff3834fded2a6043cc5ab87159dda413b204ac1
Stypox <stypox@pm.me>
2024-11-21 13:17:33 +0100

    Fix setEmptyStateComposable dark theme

--- app/src/main/java/org/schabi/newpipe/ui/emptystate/EmptyStateUtil.kt
+++ app/src/main/java/org/schabi/newpipe/ui/emptystate/EmptyStateUtil.kt
@@ -2,6 +2,10 @@
 
 package org.schabi.newpipe.ui.emptystate
 
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.contentColorFor
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import org.schabi.newpipe.ui.theme.AppTheme
@@ -14,9 +18,13 @@ fun ComposeView.setEmptyStateComposable(
     setViewCompositionStrategy(strategy)
     setContent {
         AppTheme {
-            EmptyStateComposable(
-                spec = spec
-            )
+            CompositionLocalProvider(
+                LocalContentColor provides contentColorFor(MaterialTheme.colorScheme.background)
+            ) {
+                EmptyStateComposable(
+                    spec = spec
+                )
+            }
         }
     }
 }
