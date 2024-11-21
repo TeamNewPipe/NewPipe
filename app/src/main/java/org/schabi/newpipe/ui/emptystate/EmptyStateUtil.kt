414b1a8344c573d47eece0e35f0bed04cce79159
Stypox <stypox@pm.me>
2024-11-21 13:14:19 +0100

    Remove unused methods in EmptyStateUtil

--- app/src/main/java/org/schabi/newpipe/ui/emptystate/EmptyStateUtil.kt
+++ app/src/main/java/org/schabi/newpipe/ui/emptystate/EmptyStateUtil.kt
@@ -2,14 +2,9 @@
 
 package org.schabi.newpipe.ui.emptystate
 
-import androidx.annotation.StringRes
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.State
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.compose.ui.res.stringResource
 import org.schabi.newpipe.ui.theme.AppTheme
-import androidx.compose.runtime.mutableStateOf as composeRuntimeMutableStateOf
 
 @JvmOverloads
 fun ComposeView.setEmptyStateComposable(
@@ -24,41 +19,4 @@ fun ComposeView.setEmptyStateComposable(
             )
         }
     }
-}
-
-@JvmOverloads
-fun ComposeView.setEmptyStateComposable(
-    spec: State<EmptyStateSpec>,
-    strategy: ViewCompositionStrategy = ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed,
-) = apply {
-    setViewCompositionStrategy(strategy)
-    setContent {
-        AppTheme {
-            EmptyStateComposable(
-                spec = spec.value,
-            )
-        }
-    }
-}
-
-/**
- * Used in Java land to bridge the [MutableState] API.
- */
-fun <T> mutableStateOf(param: T): MutableState<T> {
-    return composeRuntimeMutableStateOf(param)
-}
-
-/**
- * Used in Java land to modify [EmptyStateSpec] properties.
- * TODO: remove after Kotlin migration
- */
-class EmptyStateSpecBuilder(private var spec: EmptyStateSpec) {
-
-    fun descriptionText(@StringRes stringRes: Int) = apply {
-        spec = spec.copy(
-            descriptionText = { stringResource(id = stringRes) }
-        )
-    }
-
-    fun build() = spec
 }
