38a533f9a81de1d2078a0672cf0171e5c4fe12ff
Isira Seneviratne <isirasen96@gmail.com>
2025-01-15 07:48:56 +0530

    Combine ItemList code

--- app/src/main/java/org/schabi/newpipe/ui/components/items/ItemList.kt
+++ app/src/main/java/org/schabi/newpipe/ui/components/items/ItemList.kt
@@ -17,11 +17,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
+import androidx.preference.PreferenceManager
 import androidx.window.core.layout.WindowWidthSizeClass
 import my.nanihadesuka.compose.LazyVerticalGridScrollbar
+import org.schabi.newpipe.R
 import org.schabi.newpipe.info_list.ItemViewMode
 import org.schabi.newpipe.ktx.findFragmentActivity
 import org.schabi.newpipe.ui.components.common.LazyColumnThemedScrollbar
@@ -138,5 +141,25 @@ fun ItemList(
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun determineItemViewMode(): ItemViewMode {
+    val prefValue = PreferenceManager.getDefaultSharedPreferences(LocalContext.current)
+        .getString(stringResource(R.string.list_view_mode_key), null)
+    val viewMode = prefValue?.let { ItemViewMode.valueOf(it.uppercase()) } ?: ItemViewMode.AUTO
+
+    return when (viewMode) {
+        ItemViewMode.AUTO -> {
+            // Evaluate whether to use Grid based on screen real estate.
+            val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
+            if (windowSizeClass.windowWidthSizeClass == WindowWidthSizeClass.EXPANDED) {
+                ItemViewMode.GRID
+            } else {
+                ItemViewMode.LIST
+            }
+        }
+        else -> viewMode
     }
 }
