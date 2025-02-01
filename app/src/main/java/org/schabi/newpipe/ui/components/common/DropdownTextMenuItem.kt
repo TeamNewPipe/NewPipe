ac34ada7c91cde1d734042989d1a22a23c415e2c
Isira Seneviratne <isirasen96@gmail.com>
2025-02-01 05:41:02 +0530

    Make DropdownTextMenuItem non-restartable

--- app/src/main/java/org/schabi/newpipe/ui/components/common/DropdownTextMenuItem.kt
+++ app/src/main/java/org/schabi/newpipe/ui/components/common/DropdownTextMenuItem.kt
@@ -4,9 +4,11 @@ import androidx.annotation.StringRes
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.NonRestartableComposable
 import androidx.compose.ui.res.stringResource
 
 @Composable
+@NonRestartableComposable
 fun DropdownTextMenuItem(
     @StringRes text: Int,
     onClick: () -> Unit
