4130d020ae15709437c1069049e88b36c5a0a3d5
Stypox <stypox@pm.me>
2025-02-21 10:34:03 +0100

    Update app/src/main/java/org/schabi/newpipe/MainActivity.java

--- app/src/main/java/org/schabi/newpipe/MainActivity.java
+++ app/src/main/java/org/schabi/newpipe/MainActivity.java
@@ -147,7 +147,7 @@ public class MainActivity extends AppCompatActivity {
         if (DeviceUtils.supportsWebView()) {
             try {
                 new WebView(this);
-            } catch (final Exception e) {
+            } catch (final Throwable e) {
                 if (DEBUG) {
                     Log.e(TAG, "Failed to create WebView", e);
                 }
