97717df28a03c9c528859882b73fd1d3e6e53d1f
Badr Kouki <badreddinekouki1998@gmail.com>
2025-07-05 11:33:11 +0100

    fix: handle nullable nextPage behavior when searching albums #12401

--- app/src/main/java/org/schabi/newpipe/fragments/list/search/SearchFragment.java
+++ app/src/main/java/org/schabi/newpipe/fragments/list/search/SearchFragment.java
@@ -1090,7 +1090,7 @@ public class SearchFragment extends BaseListFragment<SearchInfo, ListExtractor.I
         infoListAdapter.addInfoItemList(result.getItems());
         nextPage = result.getNextPage();
 
-        if (!result.getErrors().isEmpty()) {
+        if (!result.getErrors().isEmpty() && nextPage != null) {
             showSnackBarError(new ErrorInfo(result.getErrors(), UserAction.SEARCHED,
                     "\"" + searchString + "\" → pageUrl: " + nextPage.getUrl() + ", "
                             + "pageIds: " + nextPage.getIds() + ", "
