4e88b7cd5040e4a08959120ed1d06a111c8be928
Artem Shtefan <artem@Artems-MacBook-Pro.local>
2025-08-12 20:38:44 +0300

    Improve skipping silences.
    ExoPlayer allows controlling silence skipping by adjusting the silence duration
    that is removed by `SilenceSkippingAudioProcessor`.
    The default behavior with the default parameters for this component makes it
    hard to listen to content because it leaves almost no pauses. After this commit
    it is possible to adjust silence length that remains and shorten only long
    pauses.

--- app/src/main/java/org/schabi/newpipe/player/Player.java
+++ app/src/main/java/org/schabi/newpipe/player/Player.java
@@ -72,6 +72,7 @@ import com.google.android.exoplayer2.PlaybackParameters;
 import com.google.android.exoplayer2.Player.PositionInfo;
 import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.Tracks;
+import com.google.android.exoplayer2.audio.SilenceSkippingAudioProcessor;
 import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.text.CueGroup;
@@ -293,10 +294,20 @@ public final class Player implements PlaybackListener, Listener {
                 new DefaultBandwidthMeter.Builder(context).build());
         loadController = new LoadController();
 
-        renderFactory = prefs.getBoolean(
-                context.getString(
-                        R.string.always_use_exoplayer_set_output_surface_workaround_key), false)
-                ? new CustomRenderersFactory(context) : new DefaultRenderersFactory(context);
+        final boolean alwaysUseExoplayerSetOutputSurfaceWorkaround = prefs.getBoolean(
+                context.getString(R.string.always_use_exoplayer_set_output_surface_workaround_key),
+                false);
+        final int maxSilenceDurationMillis = prefs.getInt(
+                context.getString(R.string.max_silence_duration_key),
+                Integer.parseInt(context.getString(R.string.max_silence_duration_value)));
+        final SilenceSkippingAudioProcessor silenceSkippingAudioProcessor =
+                new SilenceSkippingAudioProcessor(
+                        MILLISECONDS.toMicros(maxSilenceDurationMillis),
+                        MILLISECONDS.toMicros(maxSilenceDurationMillis),
+                        SilenceSkippingAudioProcessor.DEFAULT_SILENCE_THRESHOLD_LEVEL);
+        renderFactory = new CustomRenderersFactory(
+                context, alwaysUseExoplayerSetOutputSurfaceWorkaround,
+                silenceSkippingAudioProcessor);
 
         renderFactory.setEnableDecoderFallback(
                 prefs.getBoolean(
