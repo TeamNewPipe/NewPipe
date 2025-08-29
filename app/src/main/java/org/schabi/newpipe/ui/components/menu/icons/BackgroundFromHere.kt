fb3e28670d15e1f576095a4f70c7fcde8309e5de
Stypox <stypox@pm.me>
2025-08-29 13:00:19 +0200

    Improve icons for background/popup/play from here
    TODO: remove the commented out alternatives

--- app/src/main/java/org/schabi/newpipe/ui/components/menu/icons/BackgroundFromHere.kt
+++ app/src/main/java/org/schabi/newpipe/ui/components/menu/icons/BackgroundFromHere.kt
@@ -13,33 +13,114 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+/*
+        materialPath {
+            moveTo(12.0f, 4.0f)
+            lineToRelative(-1.41f, 1.41f)
+            lineToRelative(5.59f, 5.59f)
+            horizontalLineToRelative(-12.17f)
+            verticalLineToRelative(2.0f)
+            horizontalLineToRelative(12.17f)
+            lineToRelative(-5.59f, 5.59f)
+            lineToRelative(1.41f, 1.41f)
+            lineToRelative(8.0f, -8.0f)
+            close()
+        }
+ */
+
 /**
- * Obtained by combining Filled.Headset and Filled.PlaylistPlay
+ * Obtained by combining [androidx.compose.material.icons.filled.Headset]
+ * and the tiny arrow in [androidx.compose.material.icons.filled.ContentPasteGo].
  */
 val Icons.Filled.BackgroundFromHere: ImageVector by lazy {
-    materialIcon(name = "Filled.HeadsetPlus") {
+    materialIcon(name = "Filled.BackgroundFromHere") {
         materialPath {
-            moveTo(7.200f, 0.000f)
-            curveToRelative(-3.976f, 0.000f, -7.200f, 3.224f, -7.200f, 7.200f)
-            verticalLineToRelative(5.600f)
-            curveToRelative(0.000f, 1.328f, 1.072f, 2.400f, 2.400f, 2.400f)
-            horizontalLineToRelative(2.400f)
-            verticalLineToRelative(-6.400f)
-            horizontalLineTo(1.600f)
-            verticalLineToRelative(-1.600f)
-            curveToRelative(0.000f, -3.096f, 2.504f, -5.600f, 5.600f, -5.600f)
-            reflectiveCurveToRelative(5.600f, 2.504f, 5.600f, 5.600f)
-            verticalLineToRelative(1.600f)
-            horizontalLineToRelative(-3.200f)
-            verticalLineToRelative(6.400f)
-            horizontalLineToRelative(2.400f)
-            curveToRelative(1.328f, 0.000f, 2.400f, -1.072f, 2.400f, -2.400f)
-            verticalLineToRelative(-5.600f)
-            curveToRelative(0.000f, -3.976f, -3.224f, -7.200f, -7.200f, -7.200f)
+            moveTo(12.0f, 1.0f)
+            curveToRelative(-4.97f, 0.0f, -9.0f, 4.03f, -9.0f, 9.0f)
+            verticalLineToRelative(7.0f)
+            curveToRelative(0.0f, 1.66f, 1.34f, 3.0f, 3.0f, 3.0f)
+            horizontalLineToRelative(3.0f)
+            verticalLineToRelative(-8.0f)
+            horizontalLineTo(5.0f)
+            verticalLineToRelative(-2.0f)
+            curveToRelative(0.0f, -3.87f, 3.13f, -7.0f, 7.0f, -7.0f)
+            reflectiveCurveToRelative(7.0f, 3.13f, 7.0f, 7.0f)
+            horizontalLineToRelative(2.0f)
+            curveToRelative(0.0f, -4.97f, -4.03f, -9.0f, -9.0f, -9.0f)
             close()
         }
         materialPath {
-            moveTo(15.817f, 16.202f)
+            moveTo(19f, 11.5f)
+            lineToRelative(-1.42f, 1.41f)
+            lineToRelative(1.58f, 1.58f)
+            lineToRelative(-6.17f, 0.0f)
+            lineToRelative(0.0f, 2.0f)
+            lineToRelative(6.17f, 0.0f)
+            lineToRelative(-1.58f, 1.59f)
+            lineToRelative(1.42f, 1.41f)
+            lineToRelative(3.99f, -4.0f)
+            close()
+        }
+        /*materialPath {
+            moveTo(18.6f, 11.00f)
+            lineToRelative(-1.064f, 1.064f)
+            lineToRelative(2.586f, 2.586f)
+            horizontalLineToRelative(-5.622f)
+            verticalLineToRelative(-2.086f)
+            horizontalLineToRelative(-1.5f)
+            verticalLineToRelative(5.672f)
+            horizontalLineToRelative(1.5f)
+            verticalLineToRelative(-2.086f)
+            horizontalLineToRelative(5.622f)
+            lineToRelative(-2.586f, 2.586f)
+            lineToRelative(1.064f, 1.064f)
+            lineToRelative(4.400f, -4.400f)
+            close()
+        }*/
+        /*materialPath {
+            moveTo(18.6f, 11.00f)
+            lineToRelative(-1.064f, 1.064f)
+            lineToRelative(3.336f, 3.336f)
+            lineToRelative(-3.336f, 3.336f)
+            lineToRelative(1.064f, 1.064f)
+            lineToRelative(4.400f, -4.400f)
+            close()
+        }
+        materialPath {
+            moveTo(14f, 11.00f)
+            lineToRelative(-1.064f, 1.064f)
+            lineToRelative(3.336f, 3.336f)
+            lineToRelative(-3.336f, 3.336f)
+            lineToRelative(1.064f, 1.064f)
+            lineToRelative(4.400f, -4.400f)
+            close()
+        }*/
+        /*materialPath {
+            moveTo(18.6f, 11.00f)
+            lineToRelative(-1.064f, 1.064f)
+            lineToRelative(2.586f, 2.586f)
+            horizontalLineToRelative(-7.122f)
+            verticalLineToRelative(1.500f)
+            horizontalLineToRelative(7.122f)
+            lineToRelative(-2.586f, 2.586f)
+            lineToRelative(1.064f, 1.064f)
+            lineToRelative(4.400f, -4.400f)
+            close()
+        }*/
+        /*materialPath {
+            moveTo(17.200f, 11.200f)
+            lineToRelative(-0.775f, 0.775f)
+            lineToRelative(3.075f, 3.075f)
+            horizontalLineToRelative(-6.694f)
+            verticalLineToRelative(1.100f)
+            horizontalLineToRelative(6.694f)
+            lineToRelative(-3.075f, 3.075f)
+            lineToRelative(0.775f, 0.775f)
+            lineToRelative(4.400f, -4.400f)
+            close()
+        }*/
+        /*materialPath {
+            moveTo(12.817f, 12.202f)
             lineToRelative(-0.916f, 0.916f)
             lineToRelative(2.977f, 2.983f)
             lineToRelative(-2.977f, 2.983f)
@@ -48,14 +129,14 @@ val Icons.Filled.BackgroundFromHere: ImageVector by lazy {
             close()
         }
         materialPath {
-            moveTo(20.100f, 16.202f)
+            moveTo(17.100f, 12.202f)
             lineToRelative(-0.916f, 0.916f)
             lineToRelative(2.977f, 2.983f)
             lineToRelative(-2.977f, 2.983f)
             lineToRelative(0.916f, 0.916f)
             lineToRelative(3.900f, -3.900f)
             close()
-        }
+        }*/
     }
 }
 
--- app/src/main/java/org/schabi/newpipe/ui/components/menu/icons/PopupFromHere.kt
+++ app/src/main/java/org/schabi/newpipe/ui/components/menu/icons/PopupFromHere.kt
@@ -14,34 +14,123 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 /**
- * Obtained by combining Filled.PictureInPicture and Filled.PlaylistPlay
+ * Obtained by combining [androidx.compose.material.icons.filled.PictureInPicture]
+ * and the tiny arrow in [androidx.compose.material.icons.filled.ContentPasteGo].
  */
 val Icons.Filled.PopupFromHere: ImageVector by lazy {
-    materialIcon(name = "Filled.HeadsetPlus") {
+    materialIcon(name = "Filled.PopupFromHere") {
         materialPath {
-            moveTo(14.320f, 3.200f)
-            horizontalLineToRelative(-6.400f)
-            verticalLineToRelative(4.800f)
-            horizontalLineToRelative(6.400f)
-            lineTo(14.320f, 3.200f)
+            moveTo(19.0f, 5.0f)
+            horizontalLineToRelative(-8.0f)
+            verticalLineToRelative(5.0f)
+            horizontalLineToRelative(8.0f)
+            verticalLineToRelative(-5.0f)
             close()
-            moveTo(15.920f, 0.000f)
-            lineTo(1.520f, 0.000f)
-            curveToRelative(-0.880f, 0.000f, -1.600f, 0.720f, -1.600f, 1.600f)
-            verticalLineToRelative(11.200f)
-            curveToRelative(0.000f, 0.880f, 0.720f, 1.584f, 1.600f, 1.584f)
-            horizontalLineToRelative(14.400f)
-            curveToRelative(0.880f, 0.000f, 1.600f, -0.704f, 1.600f, -1.584f)
-            lineTo(17.520f, 1.600f)
-            curveToRelative(0.000f, -0.880f, -0.720f, -1.600f, -1.600f, -1.600f)
+            moveTo(21.0f, 1.0f)
+            horizontalLineToRelative(-18.0f)
+            curveToRelative(-1.1f, 0.0f, -2.0f, 0.9f, -2.0f, 2.0f)
+            verticalLineToRelative(14.0f)
+            curveToRelative(0.0f, 1.1f, 0.9f, 2.0f, 2.0f, 2.0f)
+            horizontalLineToRelative(8.5f)
+            verticalLineToRelative(-2.0f)
+            horizontalLineToRelative(-8.5f)
+            verticalLineToRelative(-14.0f)
+            horizontalLineToRelative(18.0f)
+            verticalLineToRelative(7.0f)
+            horizontalLineToRelative(2.0f)
+            verticalLineToRelative(-7.0f)
+            curveToRelative(0.0f, -1.1f, -0.9f, -2.0f, -2.0f, -2.0f)
             close()
-            moveTo(15.920f, 12.808f)
-            lineTo(1.520f, 12.808f)
-            lineTo(1.520f, 1.584f)
-            horizontalLineToRelative(14.400f)
-            verticalLineToRelative(11.224f)
+            /*moveTo(21.0f, 17.01f)
+            horizontalLineToRelative(-18.0f)
+            verticalLineToRelative(-14.03f)
+            horizontalLineToRelative(18.0f)
+            verticalLineToRelative(14.03f)
+            close()*/
+        }
+        materialPath {
+            moveTo(19f, 11.5f)
+            lineToRelative(-1.42f, 1.41f)
+            lineToRelative(1.58f, 1.58f)
+            lineToRelative(-6.17f, 0.0f)
+            lineToRelative(0.0f, 2.0f)
+            lineToRelative(6.17f, 0.0f)
+            lineToRelative(-1.58f, 1.59f)
+            lineToRelative(1.42f, 1.41f)
+            lineToRelative(3.99f, -4.0f)
             close()
         }
+        /*materialPath {
+            moveTo(18.6f, 11.00f)
+            lineToRelative(-1.064f, 1.064f)
+            lineToRelative(2.586f, 2.586f)
+            horizontalLineToRelative(-5.622f)
+            verticalLineToRelative(-2.086f)
+            horizontalLineToRelative(-1.5f)
+            verticalLineToRelative(5.672f)
+            horizontalLineToRelative(1.5f)
+            verticalLineToRelative(-2.086f)
+            horizontalLineToRelative(5.622f)
+            lineToRelative(-2.586f, 2.586f)
+            lineToRelative(1.064f, 1.064f)
+            lineToRelative(4.400f, -4.400f)
+            close()
+        }*/
+        /*materialPath {
+            moveTo(18.6f, 11.00f)
+            lineToRelative(-1.064f, 1.064f)
+            lineToRelative(3.336f, 3.336f)
+            lineToRelative(-3.336f, 3.336f)
+            lineToRelative(1.064f, 1.064f)
+            lineToRelative(4.400f, -4.400f)
+            close()
+        }
+        materialPath {
+            moveTo(14f, 11.00f)
+            lineToRelative(-1.064f, 1.064f)
+            lineToRelative(3.336f, 3.336f)
+            lineToRelative(-3.336f, 3.336f)
+            lineToRelative(1.064f, 1.064f)
+            lineToRelative(4.400f, -4.400f)
+            close()
+        }*/
+        /*materialPath { QUESTO È PERFETTO
+            moveTo(18.6f, 11.00f)
+            lineToRelative(-1.064f, 1.064f)
+            lineToRelative(2.586f, 2.586f)
+            horizontalLineToRelative(-7.122f)
+            verticalLineToRelative(1.500f)
+            horizontalLineToRelative(7.122f)
+            lineToRelative(-2.586f, 2.586f)
+            lineToRelative(1.064f, 1.064f)
+            lineToRelative(4.400f, -4.400f)
+            close()
+        }*/
+        /*materialPath {
+            moveTo(18.600f, 11.200f)
+            lineToRelative(-0.775f, 0.775f)
+            lineToRelative(3.075f, 3.075f)
+            horizontalLineToRelative(-6.694f)
+            verticalLineToRelative(1.100f)
+            horizontalLineToRelative(6.694f)
+            lineToRelative(-3.075f, 3.075f)
+            lineToRelative(0.775f, 0.775f)
+            lineToRelative(4.400f, -4.400f)
+            close()
+        }*/
+        /*materialPath {
+            moveTo(18.600f, 11.200f)
+            lineToRelative(-1.41f, 1.41f)
+            lineToRelative(1.99f, 1.99f)
+            horizontalLineToRelative(-6f)
+            verticalLineToRelative(2.00f)
+            horizontalLineToRelative(6f)
+            lineToRelative(-1.99f, 1.99f)
+            lineToRelative(1.41f, 1.41f)
+            lineToRelative(4.400f, -4.400f)
+            close()
+        }*/
+        /*
         materialPath {
             moveTo(15.817f, 16.202f)
             lineToRelative(-0.916f, 0.916f)
@@ -59,7 +148,7 @@ val Icons.Filled.PopupFromHere: ImageVector by lazy {
             lineToRelative(0.916f, 0.916f)
             lineToRelative(3.900f, -3.900f)
             close()
-        }
+        }*/
     }
 }
 
