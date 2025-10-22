4a5fca18cb004f6f9a1672bd196bc241a8893bc6
Jane-Yanlei-Ji <yanleiji21@gmail.com>
2025-10-23 01:13:59 +1100

    Fix formatting in HistoryRecordManager.java comments

--- app/src/main/java/org/schabi/newpipe/local/history/HistoryRecordManager.java
+++ app/src/main/java/org/schabi/newpipe/local/history/HistoryRecordManager.java
@@ -11,11 +11,11 @@ package org.schabi.newpipe.local.history;
  *
  * NewPipe is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with NewPipe.  If not, see <http://www.gnu.org/licenses/>.
+ * along with NewPipe. If not, see <http://www.gnu.org/licenses/>.
  */
 
 import static org.schabi.newpipe.util.ExtractorHelper.getStreamInfo;
