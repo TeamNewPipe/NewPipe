c6ba69aef2881e8a5c149043497675dbb5e82d1b
Yevhen Babiichuk (DustDFG) <dfgdust@gmail.com>
2025-12-28 19:16:51 +0200

    Convert newpipe/util/image/PreferredImageQuality to kotlin

---
+++ app/src/main/java/org/schabi/newpipe/util/image/PreferredImageQuality.kt
@@ -0,0 +1,33 @@
+package org.schabi.newpipe.util.image
+
+import android.content.Context
+import org.schabi.newpipe.R
+import org.schabi.newpipe.extractor.Image.ResolutionLevel
+
+enum class PreferredImageQuality {
+    NONE,
+    LOW,
+    MEDIUM,
+    HIGH;
+
+    fun toResolutionLevel(): ResolutionLevel {
+        return when (this) {
+            LOW -> ResolutionLevel.LOW
+            MEDIUM -> ResolutionLevel.MEDIUM
+            HIGH -> ResolutionLevel.HIGH
+            NONE -> ResolutionLevel.UNKNOWN
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun fromPreferenceKey(context: Context, key: String?): PreferredImageQuality {
+            return when (key) {
+                context.getString(R.string.image_quality_none_key) -> NONE
+                context.getString(R.string.image_quality_low_key) -> LOW
+                context.getString(R.string.image_quality_high_key) -> HIGH
+                else -> MEDIUM // default to medium
+            }
+        }
+    }
+}
