44cb2dc1f61b19b204c4e2ef8bd920e4420ac843
Stypox <stypox@pm.me>
2026-01-06 21:31:25 +0100

    Separate @Composables from state logic for actions editor

---
+++ app/src/main/java/org/schabi/newpipe/util/KotlinUtil.kt
@@ -0,0 +1,12 @@
+package org.schabi.newpipe.util
+
+/**
+ * Especially useful to apply some Compose Modifiers only if some condition is met. E.g.
+ * ```kt
+ * Modifier
+ *     .padding(left = 4.dp)
+ *     .letIf(someCondition) { padding(right = 4.dp) }
+ * ```
+ */
+inline fun <T> T.letIf(condition: Boolean, block: T.() -> T): T =
+    if (condition) block(this) else this
