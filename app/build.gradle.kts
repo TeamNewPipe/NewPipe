a40d7ff70e8b14b69c16fc98e23728fcdb1fec85
Stypox <stypox@pm.me>
2026-01-28 02:59:33 +0100

    Hotfix release v0.28.2 (1007)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,9 +42,9 @@ android {
         minSdk = 21
         targetSdk = 35
 
-        versionCode = System.getProperty("versionCodeOverride")?.toInt() ?: 1006
+        versionCode = System.getProperty("versionCodeOverride")?.toInt() ?: 1007
 
-        versionName = "0.28.1"
+        versionName = "0.28.2"
         System.getProperty("versionNameSuffix")?.let { versionNameSuffix = it }
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
