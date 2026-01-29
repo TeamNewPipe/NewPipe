a7aad63bbbb5b721d3c5863e69cd91bec951f30e
Aayush Gupta <aayushgupta219@gmail.com>
2026-01-29 14:59:25 +0800

    Upgrade Kotlin and KSP
    Fixes multiple build errors. Once parcelize is fixed, we should be good
    to use built-in Kotlin completely
    
    Ref: https://issuetracker.google.com/issues/478401081
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,13 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+buildscript {
+    dependencies {
+        // https://developer.android.com/build/releases/agp-9-0-0-release-notes#runtime-dependency-on-kotlin-gradle-plugin-upgrade
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.versions.kotlin.get()}")
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.legacy.kapt) apply false
