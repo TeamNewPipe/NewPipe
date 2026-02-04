8d6e68d6f43ff5b10880345d730317f289ee9def
Aayush Gupta <aayushgupta219@gmail.com>
2026-02-04 15:56:44 +0800

    Partially revert upgrade to AGP 9.0.0
    Building is broken on ecrypted filesystems
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,8 @@ import com.android.build.api.dsl.ApplicationExtension
 
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.android.legacy.kapt)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.jetbrains.kotlin.kapt)
     alias(libs.plugins.google.ksp)
     alias(libs.plugins.jetbrains.kotlin.parcelize)
     alias(libs.plugins.sonarqube)
