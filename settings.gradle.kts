cafd937688773f4441752ad370533f4670b7434c
Isira Seneviratne <isirasen96@gmail.com>
2026-05-18 08:09:11 +0530

    Update AGP

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,9 @@ pluginManagement {
         mavenCentral()
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
