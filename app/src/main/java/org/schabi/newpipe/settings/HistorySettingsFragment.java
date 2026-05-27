af5f063e6b5c79506f7fcce92682e9a1674e1afc
WizDom13 <wizdom13@gmail.com>
2026-05-27 21:40:41 +0300

    Migrate playback-state confirm dialog to MaterialAlertDialogBuilder

--- app/src/main/java/org/schabi/newpipe/settings/HistorySettingsFragment.java
+++ app/src/main/java/org/schabi/newpipe/settings/HistorySettingsFragment.java
@@ -8,6 +8,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.preference.Preference;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
 import org.schabi.newpipe.DownloaderImpl;
 import org.schabi.newpipe.R;
 import org.schabi.newpipe.error.ErrorInfo;
@@ -138,7 +140,7 @@ public class HistorySettingsFragment extends BasePreferenceFragment {
     public static void openDeletePlaybackStatesDialog(@NonNull final Context context,
                                                       final HistoryRecordManager recordManager,
                                                       final CompositeDisposable disposables) {
-        new AlertDialog.Builder(context)
+        new MaterialAlertDialogBuilder(context)
                 .setTitle(R.string.delete_playback_states_alert)
                 .setNegativeButton(R.string.cancel, ((dialog, which) -> dialog.dismiss()))
                 .setPositiveButton(R.string.delete, ((dialog, which) ->
