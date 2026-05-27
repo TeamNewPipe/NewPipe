1e0488377d70096b027bbb5d407a11f3a84e678c
WizDom13 <wizdom13@gmail.com>
2026-05-27 21:20:18 +0300

    Migrate playlist creation dialog to MaterialAlertDialogBuilder

--- app/src/main/java/org/schabi/newpipe/local/dialog/PlaylistCreationDialog.java
+++ app/src/main/java/org/schabi/newpipe/local/dialog/PlaylistCreationDialog.java
@@ -7,7 +7,8 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog.Builder;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import org.schabi.newpipe.NewPipeDatabase;
 import org.schabi.newpipe.R;
@@ -51,7 +52,7 @@ public final class PlaylistCreationDialog extends PlaylistDialog {
         dialogBinding.dialogEditText.setHint(R.string.name);
         dialogBinding.dialogEditText.setInputType(InputType.TYPE_CLASS_TEXT);
 
-        final Builder dialogBuilder = new Builder(requireContext(),
+        final MaterialAlertDialogBuilder dialogBuilder = new MaterialAlertDialogBuilder(requireContext(),
                 ThemeHelper.getDialogTheme(requireContext()))
                 .setTitle(R.string.create_playlist)
                 .setView(dialogBinding.getRoot())
