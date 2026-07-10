af29da1c26a9e9bf22b3952e3c9c1ea9600782aa
Isira Seneviratne <isirasen96@gmail.com>
2026-07-10 08:15:33 +0530

    Rm unused method

--- app/src/main/java/org/schabi/newpipe/ui/components/items/stream/StreamUtils.kt
+++ app/src/main/java/org/schabi/newpipe/ui/components/items/stream/StreamUtils.kt
@@ -1,15 +1,11 @@
 package org.schabi.newpipe.ui.components.items.stream
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
-import java.util.concurrent.TimeUnit
 import org.schabi.newpipe.extractor.Image
 import org.schabi.newpipe.extractor.stream.StreamInfoItem
 import org.schabi.newpipe.extractor.stream.StreamType
-import org.schabi.newpipe.util.Localization
 import org.schabi.newpipe.util.NO_SERVICE_ID
+import java.util.concurrent.TimeUnit
 
 @Suppress("ktlint:standard:function-naming")
 fun StreamInfoItem(
@@ -30,34 +26,6 @@ fun StreamInfoItem(
     this.duration = duration
     this.viewCount = viewCount
     this.textualUploadDate = textualUploadDate
-}
-
-@Composable
-internal fun getStreamInfoDetail(stream: StreamInfoItem): String {
-    val context = LocalContext.current
-
-    return rememberSaveable(stream) {
-        val count = stream.viewCount
-        val views = if (count >= 0) {
-            when (stream.streamType) {
-                StreamType.AUDIO_LIVE_STREAM -> Localization.listeningCount(context, count)
-                StreamType.LIVE_STREAM -> Localization.shortWatchingCount(context, count)
-                else -> Localization.shortViewCount(context, count)
-            }
-        } else {
-            ""
-        }
-        val date =
-            Localization.relativeTimeOrTextual(context, stream.uploadDate, stream.textualUploadDate)
-
-        if (views.isEmpty()) {
-            date.orEmpty()
-        } else if (date.isNullOrEmpty()) {
-            views
-        } else {
-            "$views • $date"
-        }
-    }
 }
 
 internal class StreamItemPreviewProvider : PreviewParameterProvider<StreamInfoItem> {
