94478c2cdba3f4713016b097c32894ceb429a16e
wized2 <wa0026343@gmail.com>
2026-09-18 01:55:09 +0000

    fix: null-safe license website open

--- shared/src/commonMain/kotlin/net/newpipe/app/screen/about/LicenseDialog.kt
+++ shared/src/commonMain/kotlin/net/newpipe/app/screen/about/LicenseDialog.kt
@@ -71,7 +71,7 @@ fun LicenseDialog(
         onDismissRequest = onDismiss,
         dismissButton = {
             TextButton(
-                onClick = { onOpenWebsite(license.website!!) },
+                onClick = { license.website?.let { onOpenWebsite(it) } },
                 enabled = !license.website.isNullOrBlank()
             ) {
                 Text(text = stringResource(Res.string.website_title))
